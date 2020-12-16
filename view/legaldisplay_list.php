67793c3f30dcf8bd7258aa4e69cb5d2d64f0e1c0
Nicolas <nicolas.domenech34@laposte.net>
2020-12-16 18:00:16 +0100

    Liste des AL

--- view/legaldisplay_list.php
+++ view/legaldisplay_list.php
@@ -2,6 +2,7 @@
 /* Copyright (C) 2007-2016 Laurent Destailleur  <eldy@users.sourceforge.net>
  * Copyright (C) 2014-2016 Juanjo Menent        <jmenent@2byte.es>
  * Copyright (C) 2016      Jean-François Ferry	<jfefe@aternatik.fr>
+ * Copyright (C) 2020 Eoxia <dev@eoxia.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -18,25 +19,11 @@
  */
 
 /**
- *   	\file       dev/digiriskdolibarr/digiriskdolibarr_list.php
- *		\ingroup    digiriskdolibarr othermodule1 othermodule2
- *		\brief      This file is an example of a php page
- *					Put here some comments
+ *  \file       htdocs/custom/digiriskdolibarr/view/legaldisplay.list.php
+ *  \ingroup    digiriskdolibarr
+ *  \brief      List File for legal display
  */
 
-//if (! defined('NOREQUIREUSER'))  define('NOREQUIREUSER','1');
-//if (! defined('NOREQUIREDB'))    define('NOREQUIREDB','1');
-//if (! defined('NOREQUIRESOC'))   define('NOREQUIRESOC','1');
-//if (! defined('NOREQUIRETRAN'))  define('NOREQUIRETRAN','1');
-//if (! defined('NOCSRFCHECK'))    define('NOCSRFCHECK','1');			// Do not check anti CSRF attack test
-//if (! defined('NOSTYLECHECK'))   define('NOSTYLECHECK','1');			// Do not check style html tag into posted data
-//if (! defined('NOTOKENRENEWAL')) define('NOTOKENRENEWAL','1');		// Do not check anti POST attack test
-//if (! defined('NOREQUIREMENU'))  define('NOREQUIREMENU','1');			// If there is no need to load and show top and left menu
-//if (! defined('NOREQUIREHTML'))  define('NOREQUIREHTML','1');			// If we don't need to load the html.form.class.php
-//if (! defined('NOREQUIREAJAX'))  define('NOREQUIREAJAX','1');
-//if (! defined("NOLOGIN"))        define("NOLOGIN",'1');				// If this page is public (can be called outside logged session)
-
-// Change this following line to use the correct relative path (../, ../../, etc)
 $res=0;
 if (! $res && file_exists("../main.inc.php")) $res=@include '../main.inc.php';					// to work if your module directory is into dolibarr root htdocs directory
 if (! $res && file_exists("../../main.inc.php")) $res=@include '../../main.inc.php';
@@ -51,28 +38,29 @@ require_once(DOL_DOCUMENT_ROOT.'/core/class/html.formcompany.class.php');
 require_once DOL_DOCUMENT_ROOT.'/core/lib/date.lib.php';
 require_once DOL_DOCUMENT_ROOT.'/core/lib/company.lib.php';
 dol_include_once('/digiriskdolibarr/class/legaldisplay.class.php');
+dol_include_once('/digiriskdolibarr/class/digirisk_documents.class.php');
 
 // Load traductions files requiredby by page
 $langs->load("digiriskdolibarr");
 $langs->load("other");
 
-$action=GETPOST('action','alpha');
-$massaction=GETPOST('massaction','alpha');
-$show_files=GETPOST('show_files','int');
-$confirm=GETPOST('confirm','alpha');
-$toselect = GETPOST('toselect', 'array');
+$action     = GETPOST('action','alpha');
+$massaction = GETPOST('massaction','alpha');
+$show_files = GETPOST('show_files','int');
+$confirm    = GETPOST('confirm','alpha');
+$toselect   = GETPOST('toselect', 'array');
 
-$id			= GETPOST('id','int');
+$id         = GETPOST('id','int');
 $backtopage = GETPOST('backtopage');
-$myparam	= GETPOST('myparam','alpha');
+$myparam    = GETPOST('myparam','alpha');
 
-$search_all=trim(GETPOST("sall"));
-$date_debut = GETPOST("date_debut","int");
-$ref=GETPOST("ref");
-$search_myfield=GETPOST('search_myfield');
-$optioncss = GETPOST('optioncss','alpha');
+$search_all     = trim(GETPOST("sall"));
+$date_debut     = GETPOST("date_debut","int");
+$ref            = GETPOST("ref");
+$search_myfield = GETPOST('search_myfield');
+$optioncss      = GETPOST('optioncss','alpha');
 // Load variable for pagination
-$limit = GETPOST("limit")?GETPOST("limit","int"):$conf->liste_limit;
+$limit     = GETPOST("limit")?GETPOST("limit","int"):$conf->liste_limit;
 $sortfield = GETPOST('sortfield','alpha');
 $sortorder = GETPOST('sortorder','alpha');
 //$page = GETPOST('page','int');
@@ -80,11 +68,11 @@ if ($page == -1) { $page = 0; }
 $offset = $limit * $page;
 $pageprev = $page - 1;
 $pagenext = $page + 1;
-if (! $sortfield) $sortfield="t.rowid"; // Set here default search field
-if (! $sortorder) $sortorder="ASC";
+if (! $sortfield) $sortfield = "t.rowid"; // Set here default search field
+if (! $sortorder) $sortorder = "ASC";
 
 // Protection if external user
-$socid=0;
+$socid = 0;
 if ($user->societe_id > 0)
 {
 	$socid = $user->societe_id;
@@ -92,7 +80,7 @@ if ($user->societe_id > 0)
 }
 
 // Initialize technical object to manage context to save list fields
-$contextpage=GETPOST('contextpage','aZ')?GETPOST('contextpage','aZ'):'legaldisplaylist';
+$contextpage = GETPOST('contextpage','aZ')?GETPOST('contextpage','aZ'):'legaldisplaylist';
 
 // Initialize technical object to manage hooks. Note that conf->hooks_modules contains array
 $hookmanager->initHooks(array('digiriskdolibarrlist'));
@@ -110,28 +98,11 @@ $fieldstosearchall = array(
 if (empty($user->socid)) $fieldstosearchall["t.note_private"]="NotePrivate";
 
 // Definition of fields for list
-$arrayfields=array(
+$arrayfields = array(
 	// ICI C'EST LES REF DU HEADER DE LA LISTE YOUHOU
-	't.ref'=>array('label'=>$langs->trans("Ref"), 'checked'=>1),
-	't.date_creation'=>array('label'=>$langs->trans("Date de création"), 'checked'=>0),
-	't.date_debut'=>array('label'=>$langs->trans("Date de début"), 'checked'=>1),
-	't.date_fin'=>array('label'=>$langs->trans("Date de fin"), 'checked'=>1),
-
-	't.fk_socpeople_labour_doctor'=>array('label'=>$langs->trans("Médecin du travail"), 'checked'=>1),
-	't.fk_socpeople_labour_inspector'=>array('label'=>$langs->trans("Inspecteur du travail"), 'checked'=>0),
-	't.fk_soc_samu'=>array('label'=>$langs->trans("SAMU"), 'checked'=>0),
-	't.fk_soc_police'=>array('label'=>$langs->trans("Police"), 'checked'=>0),
-	't.fk_soc_urgency'=>array('label'=>$langs->trans("Urgences"), 'checked'=>0),
-	't.fk_soc_rights_defender'=>array('label'=>$langs->trans("Défenseur du droit du travail"), 'checked'=>0),
-	't.fk_soc_antipoison'=>array('label'=>$langs->trans("Centre Antipoison"), 'checked'=>0),
-	't.fk_soc_responsible_prevent'=>array('label'=>$langs->trans("Responsable de la prévention"), 'checked'=>0),
-
-	't.description'=>array('label'=>$langs->trans("Description"), 'checked'=>0),
-	't.status'=>array('label'=>$langs->trans("Statut"), 'checked'=>0),
-	't.model_pdf'=>array('label'=>$langs->trans("Modèle PDF"), 'checked'=>0),
-	't.model_odt'=>array('label'=>$langs->trans("Modèle ODT"), 'checked'=>0),
-	't.note_affich'=>array('label'=>$langs->trans("NoteAffich"), 'checked'=>0)
-
+	't.ref' => array('label'=>$langs->trans("Ref"), 'checked'=>1),
+	't.date_creation' => array('label'=>$langs->trans("Date de création"), 'checked'=>1),
+	't.last_main_doc' => array('label'=>$langs->trans("DOC"), 'checked'=>1)
 	//'t.statut'=>array('label'=>$langs->trans("Status"), 'checked'=>1, 'position'=>1000),
 );
 // Extra fields
@@ -154,8 +125,6 @@ if (($id > 0 || ! empty($ref)) && $action != 'add')
 
 /*******************************************************************
  * ACTIONS
- *
- * Put here all code to do according to value of "action" parameter
  ********************************************************************/
 
 if (GETPOST('cancel')) { $action='list'; $massaction=''; }
@@ -192,15 +161,12 @@ if (empty($reshook))
 
 /***************************************************
  * VIEW
- *
- * Put here all code to build page
  ****************************************************/
 
-$now=dol_now();
+$now = dol_now();
 
-$form=new Form($db);
+$form = new Form($db);
 
-//$help_url="EN:Module_Customers_Orders|FR:Module_Commandes_Clients|ES:Módulo_Pedidos_de_clientes";
 $help_url='';
 $title = $langs->trans('LegalDisplayList');
 
@@ -222,99 +188,12 @@ jQuery(document).ready(function() {
 </script>';
 */
 
-$sql = "SELECT";
-$sql .= " t.rowid";
-$sql .= ", t.ref";
-$sql .= ", t.entity";
-$sql .= ", t.date_creation";
-$sql .= ", t.fk_socpeople_labour_doctor";
-$sql .= ", t.fk_socpeople_labour_inspector";
-$sql .= ", t.fk_soc_samu";
-$sql .= ", t.fk_soc_police";
-$sql .= ", t.fk_soc_urgency";
-$sql .= ", t.fk_soc_rights_defender";
-$sql .= ", t.fk_soc_antipoison";
-$sql .= ", t.fk_soc_responsible_prevent";
-$sql .= ", t.description";
-$sql .= ", t.import_key";
-$sql .= ", t.status";
-$sql .= ", t.fk_user_creat";
-$sql .= ", t.model_pdf";
-$sql .= ", t.model_odt";
-$sql .= ", t.note_affich";
-$sql .= ", s.rowid as socid, s.nom as name, s.email, s.town, s.zip, s.fk_pays, s.client, s.fournisseur, s.code_client, s.code_fournisseur, s.code_compta as code_compta_client";
-
-// Add fields from extrafields
-foreach ($extrafields->attribute_label as $key => $val) $sql.=($extrafields->attribute_type[$key] != 'separate' ? ",ef.".$key.' as options_'.$key : '');
-// Add fields from hooks
-$parameters=array();
-$reshook=$hookmanager->executeHooks('printFieldListSelect',$parameters);    // Note that $action and $object may have been modified by hook
-$sql.=$hookmanager->resPrint;
-$sql.= " FROM ".MAIN_DB_PREFIX."digirisk_legaldisplay as t";
-$sql .= " LEFT JOIN ".MAIN_DB_PREFIX."societe as s on (s.rowid  = t.fk_socpeople_labour_doctor)";
-
-if (is_array($extrafields->attribute_label) && count($extrafields->attribute_label)) $sql.= " LEFT JOIN ".MAIN_DB_PREFIX."legaldisplay_extrafields as ef on (t.rowid = ef.fk_object)";
-$sql.= " WHERE 1 = 1";
-//$sql.= " WHERE u.entity IN (".getEntity('legal_display',1).")";
-
-if ($rowid) $sql.= natural_search("rowid",$rowid);
-if ($ref) $sql.= natural_search("ref",$ref);
-
-
-
-if ($sall)          $sql.= natural_search(array_keys($fieldstosearchall), $sall);
-// Add where from extra fields
-foreach ($search_array_options as $key => $val)
-{
-	$crit=$val;
-	$tmpkey=preg_replace('/search_options_/','',$key);
-	$typ=$extrafields->attribute_type[$tmpkey];
-	$mode=0;
-	if (in_array($typ, array('int','double'))) $mode=1;    // Search on a numeric
-	if ($val && ( ($crit != '' && ! in_array($typ, array('select'))) || ! empty($crit)))
-	{
-		$sql .= natural_search('ef.'.$tmpkey, $crit, $mode);
-	}
-}
-// Add where from hooks
-$parameters=array();
-$reshook=$hookmanager->executeHooks('printFieldListWhere',$parameters);    // Note that $action and $object may have been modified by hook
-$sql.=$hookmanager->resPrint;
-$sql.=$db->order($sortfield,$sortorder);
-//$sql.= $db->plimit($conf->liste_limit+1, $offset);
-
-// Count total nb of records
-$nbtotalofrecords = '';
-if (empty($conf->global->MAIN_DISABLE_FULL_SCANLIST))
-{
-	$result = $db->query($sql);
-	$nbtotalofrecords = $db->num_rows($result);
-}
-
-$sql.= $db->plimit($limit+1, $offset);
-
-dol_syslog($script_file, LOG_DEBUG);
-$resql=$db->query($sql);
-if (! $resql)
-{
-	dol_print_error($db);
-	exit;
-}
-
-$num = $db->num_rows($resql);
-
-// Direct jump if only one record found
-if ($num == 1 && ! empty($conf->global->MAIN_SEARCH_DIRECT_OPEN_IF_ONLY_ONE) && $search_all)
-{
-	$obj = $db->fetch_object($resql);
-	$id = $obj->rowid;
-	header("Location: ".DOL_URL_ROOT.'/digiriskdolibarr/sql/legaldisplay_card.php?id='.$id);
-	exit;
-}
+$digirisk_documents = new DigiriskDocuments($db);
+$objects = $digirisk_documents->fetch_all('legaldisplay');
 
 llxHeader('', $title, $help_url);
 
-$arrayofselected=is_array($toselect)?$toselect:array();
+$arrayofselected = is_array($toselect)?$toselect:array();
 
 $param='';
 if (! empty($contextpage) && $contextpage != $_SERVER["PHP_SELF"]) $param.='&contextpage='.$contextpage;
@@ -322,13 +201,6 @@ if ($limit > 0 && $limit != $conf->liste_limit) $param.='&limit='.$limit;
 if ($rowid != '') $param.= '&amp;search_field1='.urlencode($rowid);
 if ($ref != '') $param.= '&amp;search_field2='.urlencode($ref);
 if ($optioncss != '') $param.='&optioncss='.$optioncss;
-// Add $param from extra fields
-foreach ($search_array_options as $key => $val)
-{
-	$crit=$val;
-	$tmpkey=preg_replace('/search_options_/','',$key);
-	if ($val != '') $param.='&search_options_'.$tmpkey.'='.urlencode($val);
-}
 
 $arrayofmassactions =  array(
 	'presend'=>$langs->trans("SendByMail"),
@@ -438,103 +310,57 @@ $parameters=array('arrayfields'=>$arrayfields);
 $reshook=$hookmanager->executeHooks('printFieldListOption',$parameters);    // Note that $action and $object may have been modified by hook
 print $hookmanager->resPrint;
 
-$legaldisplaystatic = new Legaldisplay($db);
-$thirdpartystatic = new Societe($db);
+$objstatic = new Legaldisplay($db);
 
+if ( ! empty( $objects ) ) {
+	foreach ( $objects as $obj ) {
 
-$i=0;
-$var=true;
-$totalarray=array();
-while ($i < min($num, $limit))
-{
-	$obj = $db->fetch_object($resql);
+		$objstatic->id = $obj->rowid;
+		$objstatic->ref = $obj->ref;
+		$objstatic->date_creation = $obj->date_creation;
+		$objstatic->last_main_doc = $obj->last_main_doc;
 
-	$legaldisplaystatic->id = $obj->rowid;
-	$legaldisplaystatic->ref = $obj->ref;
-	$legaldisplaystatic->entity = $obj->entity;
-	$legaldisplaystatic->date_creation = $obj->date_creation;
-	$legaldisplaystatic->date_debut = $db->jdate($obj->date_debut);
-	$legaldisplaystatic->date_fin = $db->jdate($obj->date_fin);
-	$legaldisplaystatic->fk_socpeople_labour_doctor = $db->jdate($obj->fk_socpeople_labour_doctor);
-	$legaldisplaystatic->fk_socpeople_labour_inspector = $db->jdate($obj->fk_socpeople_labour_inspector);
-	$legaldisplaystatic->fk_soc_samu = $db->jdate($obj->fk_soc_samu);
-	$legaldisplaystatic->fk_soc_pompiers = $db->jdate($obj->fk_soc_pompiers);
-	$legaldisplaystatic->fk_soc_police = $db->jdate($obj->fk_soc_police);
-	$legaldisplaystatic->fk_soc_urgency = $obj->fk_soc_urgency;
-	$legaldisplaystatic->description = $obj->description;
-	$legaldisplaystatic->import_key = $obj->import_key;
-	$legaldisplaystatic->status = $obj->status;
-	$legaldisplaystatic->fk_user_creat = $obj->fk_user_creat;
-	$legaldisplaystatic->model_pdf = $obj->model_pdf;
-	$legaldisplaystatic->model_odt = $obj->model_odt;
-	$legaldisplaystatic->note_affich = $obj->note_affich;
+		print '<table class="nobordernopadding"><tr class="nocellnopadd">';
 
-	$thirdpartystatic->id = $obj->fk_socpeople_labour_doctor;
-	$thirdpartystatic->name = $obj->name;
-	$thirdpartystatic->client = $obj->client;
-	$thirdpartystatic->fournisseur = $obj->fournisseur;
-	$thirdpartystatic->code_client = $obj->code_client;
-	$thirdpartystatic->code_compta_client = $obj->code_compta_client;
-	$thirdpartystatic->code_fournisseur = $obj->code_fournisseur;
-	$thirdpartystatic->code_compta_fournisseur = $obj->code_compta_fournisseur;
-	$thirdpartystatic->email = $obj->email;
-	$thirdpartystatic->country_code = $obj->country_code;
-
-	if ($obj)
-	{
-
-		$var = !$var;
-
-		// Extra fields
-		if (is_array($extrafields->attribute_label) && count($extrafields->attribute_label))
-		{
-			foreach($extrafields->attribute_label as $key => $val)
-			{
-				if (! empty($arrayfields["ef.".$key]['checked']))
-				{
-					print '<td';
-					$align=$extrafields->getAlignFlag($key);
-					if ($align) print ' align="'.$align.'"';
-					print '>';
-					$tmpkey='options_'.$key;
-					print $extrafields->showOutputField($key, $obj->$tmpkey, '', 1);
-					print '</td>';
-					if (! $i) $totalarray['nbfield']++;
-				}
-			}
-		}
-		// Fields from hook
-		/*$parameters=array('arrayfields'=>$arrayfields, 'obj'=>$obj);
-		$reshook=$hookmanager->executeHooks('printFieldListValue',$parameters);    // Note that $action and $object may have been modified by hook
-
-		print $hookmanager->resPrint;*/
 		print '<tr class="oddeven">';
-
 		// Ref
 		if (! empty($arrayfields['t.ref']['checked']))
 		{
 			print '<td class="tdoverflowmax200">';
-			print $legaldisplaystatic->getNomUrl(1);
+			print $objstatic->getNomUrl(1);
 			print '</td>';
 		}
-		// Ici mettre ref du doc odt généré
+
+		// Name
+		if (! empty($arrayfields['t.last_main_doc']['checked']))
+		{
+			print '<td class="tdoverflowmax200">';
+			print $objstatic->last_main_doc;
+			print '</td>';
+		}
+
+		// Date de création
+		if (! empty($arrayfields['t.date_creation']['checked']))
+		{
+			print '<td class="tdoverflowmax200">';
+			print $objstatic->date_creation;
+			print '</td>';
+		}
+		print '</tr>';
+	}
+}
+
 
 		// Action column
-		print '<td class="wrap" align="center">';
-
-		if ($massactionbutton || $massaction)   // If we are in select mode (massactionbutton defined) or if we have already selected and sent an action ($massaction) defined
-		{
-			$selected=0;
-			if (in_array($obj->rowid, $arrayofselected)) $selected=1;
-			print '<input id="cb'.$obj->rowid.'" class="flat checkforselect" type="checkbox" name="toselect[]" value="'.$obj->rowid.'"'.($selected?' checked="checked"':'').'>';
-		}
-		print '</td>';
-		if (! $i) $totalarray['nbfield']++;
-		print '</tr>';
-
-	}
-	$i++;
-}
+//		print '<td class="wrap" align="center">';
+//
+//		if ($massactionbutton || $massaction)   // If we are in select mode (massactionbutton defined) or if we have already selected and sent an action ($massaction) defined
+//		{
+//			$selected=0;
+//			if (in_array($obj->rowid, $arrayofselected)) $selected=1;
+//			print '<input id="cb'.$obj->rowid.'" class="flat checkforselect" type="checkbox" name="toselect[]" value="'.$obj->rowid.'"'.($selected?' checked="checked"':'').'>';
+//		}
+//		print '</td>';
 
 // Show total line
 if (isset($totalarray['date_creation']))
