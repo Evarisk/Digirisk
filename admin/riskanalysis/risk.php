f3b796a4b286bffcb67b63106797597c085b46f0
Nicolas <nicolas@eoxia.com>
2021-04-07 18:14:12 +0200

    add: New organisation/Clean code/Configuration for admin page

---
+++ admin/riskanalysis/risk.php
@@ -0,0 +1,201 @@
+<?php
+/* Copyright (C) 2021 EOXIA <dev@eoxia.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * \file    digiriskdolibarr/admin/risk.php
+ * \ingroup digiriskdolibarr
+ * \brief   Digiriskdolibarr risk page.
+ */
+
+// Load Dolibarr environment
+$res = 0;
+if (!$res && file_exists("../../../../main.inc.php")) $res = @include "../../../../main.inc.php";
+if (!$res) die("Include of main fails");
+
+global $langs, $user;
+
+// Libraries
+require_once DOL_DOCUMENT_ROOT . "/core/lib/admin.lib.php";
+require_once DOL_DOCUMENT_ROOT . "/core/class/html.formprojet.class.php";
+dol_include_once('/custom/digiriskdolibarr/lib/digiriskdolibarr.lib.php');
+dol_include_once('/custom/digiriskdolibarr/class/digiriskdocuments.class.php');
+
+// Translations
+$langs->loadLangs(array("digiriskdolibarr@digiriskdolibarr"));
+
+// Access control
+if (!$user->admin) accessforbidden();
+
+// Parameters
+$action     = GETPOST('action', 'alpha');
+$backtopage = GETPOST('backtopage', 'alpha');
+$value      = GETPOST('value', 'alpha');
+
+$type          = 'risk';
+$error         = 0;
+$setupnotempty = 0;
+
+/*
+ * Actions
+ */
+if ($action == 'updateMask')
+{
+	$maskconstrisk = GETPOST('maskconstrisk', 'alpha');
+	$maskrisk      = GETPOST('maskrisk', 'alpha');
+
+	if ($maskconstrisk) $res = dolibarr_set_const($db, $maskconstrisk, $maskrisk, 'chaine', 0, '', $conf->entity);
+
+	if (!$res > 0) $error++;
+
+	if (!$error)
+	{
+		setEventMessages($langs->trans("SetupSaved"), null, 'mesgs');
+	} else {
+		setEventMessages($langs->trans("Error"), null, 'errors');
+	}
+}
+
+if ($action == 'setmod')
+{
+	$constforval = 'DIGIRISKDOLIBARR_'.strtoupper($type)."_ADDON";
+	dolibarr_set_const($db, $constforval, $value, 'chaine', 0, '', $conf->entity);
+}
+
+/*
+ * View
+ */
+$form            = new Form($db);
+
+$help_url  = 'FR:Module_DigiriskDolibarr';
+$page_name = "DigiriskdolibarrSetup";
+
+llxHeader('', $langs->trans($page_name), $help_url);
+
+// Subheader
+$linkback = '<a href="'.($backtopage ? $backtopage : DOL_URL_ROOT.'/admin/modules.php?restore_lastsearch_values=1').'">'.$langs->trans("BackToModuleList").'</a>';
+
+print load_fiche_titre($langs->trans($page_name), $linkback, 'object_digiriskdolibarr@digiriskdolibarr');
+
+$head = digiriskdolibarrAdminPrepareHead();
+dol_fiche_head($head, 'riskanalysis', '', -1, "digiriskdolibarr@digiriskdolibarr");
+$head = digiriskdolibarrAdminRiskAnalysisPrepareHead();
+dol_fiche_head($head, 'risk', '', -1, "digiriskdolibarr@digiriskdolibarr");
+
+/*
+ *  Numbering module
+ */
+
+print load_fiche_titre($langs->trans("DigiriskRiskNumberingModule"), '', '');
+
+print '<table class="noborder centpercent">';
+print '<tr class="liste_titre">';
+print '<td>'.$langs->trans("Name").'</td>';
+print '<td>'.$langs->trans("Description").'</td>';
+print '<td class="nowrap">'.$langs->trans("Example").'</td>';
+print '<td class="center" width="60">'.$langs->trans("Status").'</td>';
+print '<td class="center" width="16">'.$langs->trans("ShortInfo").'</td>';
+print '</tr>'."\n";
+
+clearstatcache();
+
+$dir = dol_buildpath("/custom/digiriskdolibarr/core/modules/digiriskdolibarr/riskanalysis/".$type."/");
+if (is_dir($dir))
+{
+	$handle = opendir($dir);
+	if (is_resource($handle))
+	{
+		while (($file = readdir($handle)) !== false )
+		{
+			if (!is_dir($dir.$file) || (substr($file, 0, 1) <> '.' && substr($file, 0, 3) <> 'CVS'))
+			{
+				$filebis = $file;
+
+				$classname = preg_replace('/\.php$/', '', $file);
+				$classname = preg_replace('/\-.*$/', '', $classname);
+
+				if (!class_exists($classname) && is_readable($dir.$filebis) && (preg_match('/mod_/', $filebis) || preg_match('/mod_/', $classname)) && substr($filebis, dol_strlen($filebis) - 3, 3) == 'php')
+				{
+					// Charging the numbering class
+					require_once $dir.$filebis;
+
+					$module = new $classname($db);
+
+					if ($module->isEnabled())
+					{
+						print '<tr class="oddeven"><td width="100">';
+						print $langs->trans($module->name);
+						print "</td><td>\n";
+						print $module->info();
+						print '</td>';
+
+						// Show example of numbering module
+						print '<td class="nowrap">';
+						$tmp = $module->getExample();
+						if (preg_match('/^Error/', $tmp)) print '<div class="error">'.$langs->trans($tmp).'</div>';
+						elseif ($tmp == 'NotConfigured') print $langs->trans($tmp);
+						else print $tmp;
+						print '</td>'."\n";
+
+						print '<td class="center">';
+						if ($conf->global->DIGIRISKDOLIBARR_RISK_ADDON == $file || $conf->global->DIGIRISKDOLIBARR_RISK_ADDON.'.php' == $file)
+						{
+							print img_picto($langs->trans("Activated"), 'switch_on');
+						}
+						else
+						{
+							print '<a class="reposition" href="'.$_SERVER["PHP_SELF"].'?action=setmod&value='.preg_replace('/\.php$/', '', $file).'&scan_dir='.$module->scandir.'&label='.urlencode($module->name).'" alt="'.$langs->trans("Default").'">'.img_picto($langs->trans("Disabled"), 'switch_off').'</a>';
+						}
+						print '</td>';
+
+						// Example for listing risks action
+						$htmltooltip = '';
+						$htmltooltip .= ''.$langs->trans("Version").': <b>'.$module->getVersion().'</b><br>';
+						$nextval = $module->getNextValue($object_document);
+						if ("$nextval" != $langs->trans("NotAvailable")) {  // Keep " on nextval
+							$htmltooltip .= $langs->trans("NextValue").': ';
+							if ($nextval) {
+								if (preg_match('/^Error/', $nextval) || $nextval == 'NotConfigured')
+									$nextval = $langs->trans($nextval);
+								$htmltooltip .= $nextval.'<br>';
+							} else {
+								$htmltooltip .= $langs->trans($module->error).'<br>';
+							}
+						}
+
+						print '<td class="center">';
+						print $form->textwithpicto('', $htmltooltip, 1, 0);
+						if ($conf->global->DIGIRISKDOLIBARR_RISK_ADDON.'.php' == $file)  // If module is the one used, we show existing errors
+						{
+							if (!empty($module->error)) dol_htmloutput_mesg($module->error, '', 'error', 1);
+						}
+						print '</td>';
+						print "</tr>\n";
+					}
+				}
+			}
+		}
+		closedir($handle);
+	}
+}
+
+print '</table>';
+
+// Page end
+dol_fiche_end();
+
+llxFooter();
+$db->close();
--- admin/riskanalysis/task.php
+++ admin/riskanalysis/task.php
@@ -1,9 +1,9 @@
 <?php
-/* Copyright (C) 2018       Alexandre Spangaro      <aspangaro@open-dsi.fr>
+/* Copyright (C) 2021 EOXIA <dev@eoxia.com>
  *
- * This program is free software; you can redistribute it and/or modify
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
+ * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
@@ -12,51 +12,51 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 /**
- *	\file       htdocs/admin/accountant.php
- *	\ingroup    accountant
- *	\brief      Setup page to configure accountant / auditor
+ * \file    digiriskdolibarr/admin/task.php
+ * \ingroup digiriskdolibarr
+ * \brief   Digiriskdolibarr task page.
  */
 
-require '../../../main.inc.php';
-require_once DOL_DOCUMENT_ROOT.'/core/lib/admin.lib.php';
-require_once '../lib/digiriskdolibarr.lib.php';
-require_once DOL_DOCUMENT_ROOT.'/custom/digiriskdolibarr/class/digiriskresources.class.php';
-require_once DOL_DOCUMENT_ROOT.'/core/class/html.formprojet.class.php';
+// Load Dolibarr environment
+$res = 0;
+if (!$res && file_exists("../../../../main.inc.php")) $res = @include "../../../../main.inc.php";
+if (!$res) die("Include of main fails");
 
-$action = GETPOST('action', 'aZ09');
-$contextpage = GETPOST('contextpage', 'aZ') ?GETPOST('contextpage', 'aZ') : 'adminsocial'; // To manage different context of search
+global $langs, $user;
 
-// Load translation files required by the page
-$langs->loadLangs(array('admin', 'companies'));
+// Libraries
+require_once DOL_DOCUMENT_ROOT . "/core/lib/admin.lib.php";
+require_once DOL_DOCUMENT_ROOT . "/core/class/html.formprojet.class.php";
+dol_include_once('/custom/digiriskdolibarr/lib/digiriskdolibarr.lib.php');
+dol_include_once('/custom/digiriskdolibarr/class/digiriskdocuments.class.php');
 
+// Translations
+$langs->loadLangs(array("digiriskdolibarr@digiriskdolibarr"));
+
+// Access control
 if (!$user->admin) accessforbidden();
 
-$error = 0;
-$hookmanager->initHooks(array('globaladmin'));
+// Parameters
+$action     = GETPOST('action', 'alpha');
+$backtopage = GETPOST('backtopage', 'alpha');
+$value      = GETPOST('value', 'alpha');
 
-global $conf, $db;
+$error         = 0;
+$setupnotempty = 0;
 
 /*
  * Actions
  */
-
-$parameters = array();
-$reshook = $hookmanager->executeHooks('doActions', $parameters, $object, $action);
-if ($reshook < 0) setEventMessages($hookmanager->error, $hookmanager->errors, 'errors');
-
-if (($action == 'update' && !GETPOST("cancel", 'alpha'))
-	|| ($action == 'updateedit'))
+if (($action == 'update' && !GETPOST("cancel", 'alpha')) || ($action == 'updateedit'))
 {
-
 	$projectLinked = GETPOST('projectLinked', 'none');
 	$projectLinked  = preg_split('/_/', $projectLinked);
 
 	dolibarr_set_const($db, "DIGIRISKDOLIBARR_PROJECT_LINKED", $projectLinked[0], 'integer', 0, '', $conf->entity);
-
 
 	if ($action != 'updateedit' && !$error)
 	{
@@ -68,11 +68,11 @@ if (($action == 'update' && !GETPOST("cancel", 'alpha'))
 /*
  * View
  */
-
 if (!empty($conf->projet->enabled)) { $formproject = new FormProjets($db); }
 
-$help_url = 'FR:Module_DigiriskDolibarr';
+$help_url  = 'FR:Module_DigiriskDolibarr';
 $page_name = "DigiriskdolibarrSetup";
+
 llxHeader('', $langs->trans($page_name), $help_url);
 
 // Subheader
@@ -80,17 +80,22 @@ $linkback = '<a href="'.($backtopage ? $backtopage : DOL_URL_ROOT.'/admin/module
 
 print load_fiche_titre($langs->trans($page_name), $linkback, 'object_digiriskdolibarr@digiriskdolibarr');
 
-// Configuration header
 $head = digiriskdolibarrAdminPrepareHead();
-dol_fiche_head($head, 'risk', '', -1, "digiriskdolibarr@digiriskdolibarr");
+dol_fiche_head($head, 'riskanalysis', '', -1, "digiriskdolibarr@digiriskdolibarr");
+$head = digiriskdolibarrAdminRiskAnalysisPrepareHead();
+dol_fiche_head($head, 'task', '', -1, "digiriskdolibarr@digiriskdolibarr");
 
-print '<span class="opacitymedium">'.$langs->trans("DigiriskMenu")."</span><br>\n";
-print "<br>\n";
+print load_fiche_titre($langs->trans("TasksManagement"), '', '');
+
 print '<form method="POST" action="'.$_SERVER["PHP_SELF"].'" name="social_form">';
 print '<input type="hidden" name="token" value="'.newToken().'">';
 print '<input type="hidden" name="action" value="update">';
 print '<table class="noborder centpercent editmode">';
-print '<tr class="liste_titre"><th class="titlefield wordbreak">'.$langs->trans("TasksManagement").'</th><th>'.$langs->trans("").'</th></tr>'."\n";
+print '<tr class="liste_titre">';
+print '<td>'.$langs->trans("Name").'</td>';
+print '<td>'.$langs->trans("SelectProject").'</td>';
+print '<td>'.$langs->trans("Action").'</td>';
+print '</tr>'."\n";
 
 // Project
 if (!empty($conf->projet->enabled))
@@ -99,14 +104,15 @@ if (!empty($conf->projet->enabled))
 	print '<tr class="oddeven"><td><label for="projectLinked">'.$langs->trans("ProjectLinked").'</label></td><td>';
 	$numprojet = $formproject->select_projects(0,  $conf->global->DIGIRISKDOLIBARR_PROJECT_LINKED, 'projectLinked', 0, 0, 0, 0, 0, 0, 0, '', 0, 0, 'maxwidth500');
 	print ' <a href="'.DOL_URL_ROOT.'/projet/card.php?socid='.$soc->id.'&action=create&status=1&backtopage='.urlencode($_SERVER["PHP_SELF"].'?action=create&socid='.$soc->id).'"><span class="fa fa-plus-circle valignmiddle" title="'.$langs->trans("AddProject").'"></span></a>';
+	print '<td><input type="submit" class="button" name="save" value="'.$langs->trans("Save").'">';
 	print '</td></tr>';
 }
 
 print '</table>';
-print '<br><div class="center">';
-print '<input type="submit" class="button" name="save" value="'.$langs->trans("Save").'">';
-print '</div>';
 print '</form>';
+
+// Page end
+dol_fiche_end();
 
 llxFooter();
 $db->close();
