709b434af163e9fb9ca356af2a81a3f19d209efb
Nicolas <nicolas@eoxia.com>
2021-04-29 12:10:07 +0200

    [Loader] add: eoframework loader design

--- js/digiriskdolibarr.js.php
+++ js/digiriskdolibarr.js.php
@@ -721,6 +721,88 @@ if ( ! window.eoxiaJS.tooltip ) {
 }
 
 /**
+ * @namespace EO_Framework_Loader
+ *
+ * @author Eoxia <dev@eoxia.com>
+ * @copyright 2015-2018 Eoxia
+ */
+
+/*
+ * Gestion du loader.
+ *
+ * @since 1.0.0
+ * @version 1.0.0
+ */
+if ( ! window.eoxiaJS.loader ) {
+
+	/**
+	 * [loader description]
+	 *
+	 * @memberof EO_Framework_Loader
+	 *
+	 * @type {Object}
+	 */
+	window.eoxiaJS.loader = {};
+
+	/**
+	 * [description]
+	 *
+	 * @memberof EO_Framework_Loader
+	 *
+	 * @returns {void} [description]
+	 */
+	window.eoxiaJS.loader.init = function() {
+		window.eoxiaJS.loader.event();
+	};
+
+	/**
+	 * [description]
+	 *
+	 * @memberof EO_Framework_Loader
+	 *
+	 * @returns {void} [description]
+	 */
+	window.eoxiaJS.loader.event = function() {
+	};
+
+	/**
+	 * [description]
+	 *
+	 * @memberof EO_Framework_Loader
+	 *
+	 * @param  {void} element [description]
+	 * @returns {void}         [description]
+	 */
+	window.eoxiaJS.loader.display = function( element ) {
+		// Loader spécial pour les "button-progress".
+		if ( element.hasClass( 'button-progress' ) ) {
+			element.addClass( 'button-load' )
+		} else {
+			element.addClass( 'wpeo-loader' );
+			var el = jQuery( '<span class="loader-spin"></span>' );
+			element[0].loaderElement = el;
+			element.append( element[0].loaderElement );
+		}
+	};
+
+	/**
+	 * [description]
+	 *
+	 * @memberof EO_Framework_Loader
+	 *
+	 * @param  {void} element [description]
+	 * @returns {void}         [description]
+	 */
+	window.eoxiaJS.loader.remove = function( element ) {
+		if ( 0 < element.length && ! element.hasClass( 'button-progress' ) ) {
+			element.removeClass( 'wpeo-loader' );
+
+			jQuery( element[0].loaderElement ).remove();
+		}
+	};
+}
+
+/**
  * Initialise l'objet "photo" ainsi que la méthode "init" obligatoire pour la bibliothèque EoxiaJS.
  *
  * @since   1.0.0
@@ -814,8 +896,15 @@ window.eoxiaJS.photo.sendPhoto = function( event ) {
 		data: formdata,
 		processData: false,
 		contentType: false,
-		success: $(this).closest('.modal-container').find('.ecm-photo-list').load( document.URL + ' .ecm-photo-list')
 	});
+
+	$(this).closest('.modal-container').find('.ecm-photo-list').empty();
+	window.eoxiaJS.loader.display($(this).closest('.modal-container').find('.ecm-photo-list'));
+	let elementParent = $(this).closest('.modal-container').find('.ecm-photo-list');
+	elementParent.load( document.URL + ' .ecm-photo-list-');
+	setTimeout(function(){
+		elementParent.removeClass('wpeo-loader');
+	}, 1000);
 };
 
 /**
