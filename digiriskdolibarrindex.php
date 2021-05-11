8cdade7b1809299087ec66ab2b9b66b14f70df39
Nicolas <nicolas@eoxia.com>
2021-05-11 15:01:08 +0200

    [Projet] fix : rework with date function dolibarr

--- digiriskdolibarrindex.php
+++ digiriskdolibarrindex.php
@@ -60,13 +60,17 @@ if ( $conf->global->DIGIRISKDOLIBARR_DU_PROJECT == 0 || $project->statut == 2 ) 
 	$project->title       = $langs->trans('RiskAssessmentDocument');
 	$project->description = $langs->trans('RiskAssessmentDocumentDescription');
 	$project->date_c      = dol_now();
-	$currentYear = date('Y');
-	$fiscalMonthStart = $conf->global->SOCIETE_FISCAL_MONTH_START;
-	$startdate = $currentYear . '-' . $fiscalMonthStart . '-01';
-	$project->date_start = $startdate;
-	$project->usage_task  = 1;
-	$enddate = date('Y-m-d', strtotime(date("Y-m-d", strtotime($startdate)) .  " + 1 year - 1 days"));
-	$project->date_end = $enddate;
+	$currentYear          = dol_print_date(dol_now(),'%Y');
+	$fiscalMonthStart     = $conf->global->SOCIETE_FISCAL_MONTH_START;
+	$startdate            = dol_mktime('0','0','0',$fiscalMonthStart ? $fiscalMonthStart : '1', '1', $currentYear);
+	$project->date_start  = $startdate;
+
+	$project->usage_task = 1;
+
+	$startdateAddYear      = dol_time_plus_duree($startdate, 1,'y');
+	$startdateAddYearMonth = dol_time_plus_duree($startdateAddYear, -1,'d');
+	$enddate               = dol_print_date($startdateAddYearMonth, 'dayrfc');
+	$project->date_end     = $enddate;
 	$project->statut      = 1;
 	$project_id = $project->create($user);
 	dolibarr_set_const($db, 'DIGIRISKDOLIBARR_DU_PROJECT', $project_id, 'integer', 1, '',$conf->entity);
