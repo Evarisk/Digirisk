51c2ded302bd418656a7682b841f7245faa23bed
Nicolas <nicolas@eoxia.com>
2021-06-23 18:11:55 +0200

    [DigiriskSignature] add : clean code and new status fonctionnality

--- preventionplan_attendants.php
+++ preventionplan_attendants.php
@@ -16,9 +16,9 @@
  */
 
 /**
- *   	\file       preventionplan_card.php
+ *   	\file       preventionplan_attendants.php
  *		\ingroup    digiriskdolibarr
- *		\brief      Page to create/edit/view preventionplan
+ *		\brief      Page to create/edit/view preventionplan_signature
  */
 
 // Load Dolibarr environment
@@ -35,58 +35,36 @@ if (!$res && file_exists("../../main.inc.php")) $res = @include "../../main.inc.
 if (!$res && file_exists("../../../main.inc.php")) $res = @include "../../../main.inc.php";
 if (!$res) die("Include of main fails");
 
-require_once DOL_DOCUMENT_ROOT.'/core/class/html.formfile.class.php';
-
-require_once __DIR__ . '/class/digiriskdocuments.class.php';
-require_once __DIR__ . '/class/digiriskelement.class.php';
 require_once __DIR__ . '/class/digiriskresources.class.php';
 require_once __DIR__ . '/class/preventionplan.class.php';
-require_once __DIR__ . '/class/riskanalysis/risk.class.php';
-require_once __DIR__ . '/class/digiriskdocuments/preventionplandocument.class.php';
-require_once __DIR__ . '/lib/digiriskdolibarr_function.lib.php';
 require_once __DIR__ . '/lib/digiriskdolibarr_preventionplan.lib.php';
-require_once __DIR__ . '/core/modules/digiriskdolibarr/digiriskelement/preventionplan/mod_preventionplan_standard.php';
-require_once __DIR__ . '/core/modules/digiriskdolibarr/digiriskelement/preventionplandet/mod_preventionplandet_standard.php';
-require_once __DIR__ . '/core/modules/digiriskdolibarr/digiriskdocuments/preventionplandocument/mod_preventionplandocument_standard.php';
-require_once __DIR__ . '/core/modules/digiriskdolibarr/digiriskdocuments/preventionplandocument/modules_preventionplandocument.php';
 
-global $db, $conf, $langs;
+global $db, $langs;
 
 // Load translation files required by the page
 $langs->loadLangs(array("digiriskdolibarr@digiriskdolibarr", "other"));
 
 // Get parameters
 $id                  = GETPOST('id', 'int');
-$lineid              = GETPOST('lineid', 'int');
-$ref                 = GETPOST('ref', 'alpha');
 $action              = GETPOST('action', 'aZ09');
-$confirm             = GETPOST('confirm', 'alpha');
-$cancel              = GETPOST('cancel', 'aZ09');
-$contextpage         = GETPOST('contextpage', 'aZ') ?GETPOST('contextpage', 'aZ') : 'preventionplancard'; // To manage different context of search
+$contextpage         = GETPOST('contextpage', 'aZ') ?GETPOST('contextpage', 'aZ') : 'preventionplansignature'; // To manage different context of search
 $backtopage          = GETPOST('backtopage', 'alpha');
-$backtopageforcancel = GETPOST('backtopageforcancel', 'alpha');
-$fk_parent           = GETPOST('fk_parent', 'int');
 
 // Initialize technical objects
-$object = new PreventionPlan($db);
-$signatory = new PreventionPlanSignature($db);
-
-$usertmp = new User($db);
-$contact = new Contact($db);
+$object            = new PreventionPlan($db);
+$signatory         = new PreventionPlanSignature($db);
+$digiriskresources = new DigiriskResources($db);
+$usertmp           = new User($db);
+$contact           = new Contact($db);
 
 $object->fetch($id);
 
-$digiriskelement   = new DigiriskElement($db);
-$digiriskresources = new DigiriskResources($db);
-
 $hookmanager->initHooks(array('preventionplansignature', 'globalcard')); // Note that conf->hooks_modules contains array
 
-$upload_dir         = $conf->digiriskdolibarr->multidir_output[isset($object->entity) ? $object->entity : 1];
 $permissiontoread   = $user->rights->digiriskdolibarr->preventionplandocument->read;
-$permissiontoadd    = $user->rights->digiriskdolibarr->preventionplandocument->write;
-$permissiontodelete = $user->rights->digiriskdolibarr->preventionplandocument->delete;
 
 if (!$permissiontoread) accessforbidden();
+
 /*
 /*
  * Actions
@@ -98,7 +76,6 @@ if ($reshook < 0) setEventMessages($hookmanager->error, $hookmanager->errors, 'e
 
 // Action to add record
 if ($action == 'addSignature') {
-
 	$signatoryID = GETPOST('signatoryID');
 	$signature = GETPOST('signature');
 	$request_body = file_get_contents('php://input');
@@ -109,6 +86,7 @@ if ($action == 'addSignature') {
 	if (!$error) {
 		$result = $signatory->update($user, false);
 		if ($result > 0) {
+			$signatory->setSigned($user, false);
 			// Creation signature OK
 			$urltogo = str_replace('__ID__', $result, $backtopage);
 			$urltogo = preg_replace('/--IDFORBACKTOPAGE--/', $id, $urltogo); // New method to autoselect project after a New on another form object creation
@@ -129,7 +107,7 @@ if ($action == 'addSignature') {
  */
 
 $title    = $langs->trans("PreventionPlan");
-$help_url = 'EN:Module_Third_Parties|FR:Module_DigiriskDolibarr#L.27onglet_Horaire_de_travail|ES:Empresas';
+$help_url = '';
 $morejs   = array("/digiriskdolibarr/js/signature-pad.min.js", "/digiriskdolibarr/js/digiriskdolibarr.js.php");
 $morecss  = array("/digiriskdolibarr/css/digiriskdolibarr.css");
 
@@ -149,14 +127,21 @@ print '<div class="underbanner clearboth"></div>';
 
 print dol_get_fiche_end();
 
-print load_fiche_titre($langs->trans("SignatureManagement"), '', '');
+print '<div class="div-table-responsive">'; ?>
 
-print '<div class="div-table-responsive">';
+<div class="wpeo-notice notice-warning">
+	<div class="notice-content">
+		<div class="notice-title"><?php echo $langs->trans('DisclaimerSignatureTitle') ?></div>
+		<div class="notice-subtitle"><?php echo $langs->trans("DisclaimerSignature") ?></div>
+	</div>
+</div>
+
+<?php print load_fiche_titre($langs->trans("SignatureResponsibles"), '', '');
 
 print '<table class="border centpercent tableforfield">';
 print '<tr class="liste_titre">';
+print '<td>'.$langs->trans("Role").'</td>';
 print '<td>'.$langs->trans("Name").'</td>';
-print '<td>'.$langs->trans("People").'</td>';
 print '<td class="center">'.$langs->trans("Action").'</td>';
 print '<td>'.$langs->trans("Status").'</td>';
 print '</tr>'."\n";
@@ -164,207 +149,64 @@ print '</tr>'."\n";
 //Master builder -- Maitre Oeuvre
 print '<tr class="oddeven"><td>';
 print $langs->trans("MaitreOeuvre");
-print '</td>';
-print '<td>';
+print '</td><td>';
 $element = $signatory->fetchSignatory('PP_MAITRE_OEUVRE', $id);
-
 if ($element > 0) {
 	$element = array_shift($element);
 	$usertmp->fetch($element->element_id);
 	print $usertmp->getNomUrl(1);
 }
-print '<br>';
-print '</td>';
-print '<td class="center">'; ?>
-
-<?php if (empty($element->signature)) : ?>
-	<div class="wpeo-button button-blue wpeo-modal-event modal-signature-open modal-open" value="<?php echo $element->id ?>">
-		<span><?php echo $langs->trans('Sign'); ?></span>
-	</div>
-
-	<div class="modal-signature" value="<?php echo $element->id ?>">
-		<div class="wpeo-modal modal-signature" id="modal-signature<?php echo $element->id ?>">
-			<div class="modal-container wpeo-modal-event">
-				<!-- Modal-Header-->
-				<div class="modal-header">
-					<h2 class="modal-title"><?php echo $langs->trans('Signature'); ?></h2>
-					<div class="modal-close"><i class="fas fa-times"></i></div>
-				</div>
-				<!-- Modal-ADD Signature Content-->
-				<div class="modal-content" id="#modalContent">
-					<canvas style="height: 95%; width: 95%; border: #0b419b solid 2px"></canvas>
-				</div>
-				<!-- Modal-Footer-->
-				<div class="modal-footer">
-					<div class="signature-erase wpeo-button button-grey">
-						<span><i class="fas fa-eraser"></i> <?php echo $langs->trans('Erase'); ?></span>
-					</div>
-					<div class="wpeo-button button-grey modal-close">
-						<span><?php echo $langs->trans('Cancel'); ?></span>
-					</div>
-					<div class="signature-validate wpeo-button button-primary" value="<?php echo $element->id ?>">
-						<span><?php echo $langs->trans('Validate'); ?></span>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-<?php else : ?>
-	<img src='<?php echo $element->signature ?>' width="100px" height="100px" style="border: #0b419b solid 2px">
-<?php endif; ?>
-
-<?php
-print '</td>';
-print '<td>';
-print $element->status;
+print '</td><td class="center">';
+require __DIR__ . "/core/tpl/digiriskdolibarr_signature_view.tpl.php";
+print '</td><td>';
+print $element->getLibStatut(5);
 print '</td></tr>';
 
 //External Society Responsible -- Responsable Société extérieure
 print '<tr class="oddeven"><td>';
 print $langs->trans("ExtSocietyResponsible");
-print '</td>';
-print '<td>';
-$ext_society_responsible = $signatory->fetchSignatory('PP_EXT_SOCIETY_RESPONSIBLE', $id);
-
-if ($ext_society_responsible > 0) {
-	$ext_society_responsible = array_shift($ext_society_responsible);
-	$contact->fetch($ext_society_responsible->element_id);
+print '</td><td>';
+$element = $signatory->fetchSignatory('PP_EXT_SOCIETY_RESPONSIBLE', $id);
+if ($element > 0) {
+	$element = array_shift($element);
+	$contact->fetch($element->element_id);
 	print $contact->getNomUrl(1);
 }
-print '<br>';
-print '</td>';
-print '<td class="center">'; ?>
-<?php if (empty($ext_society_responsible->signature)) : ?>
-	<div class="wpeo-button button-blue wpeo-modal-event modal-signature-open modal-open" value="<?php echo $ext_society_responsible->id ?>">
-		<span><?php echo $langs->trans('Sign'); ?></span>
-	</div>
-
-	<div class="modal-signature" value="<?php echo $ext_society_responsible->id ?>">
-		<div class="wpeo-modal modal-signature" id="modal-signature<?php echo $ext_society_responsible->id ?>">
-			<div class="modal-container wpeo-modal-event">
-				<!-- Modal-Header-->
-				<div class="modal-header">
-					<h2 class="modal-title"><?php echo $langs->trans('Signature'); ?></h2>
-					<div class="modal-close"><i class="fas fa-times"></i></div>
-				</div>
-				<!-- Modal-ADD Signature Content-->
-				<div class="modal-content" id="#modalContent">
-					<canvas style="height: 95%; width: 95%; border: #0b419b solid 2px"></canvas>
-				</div>
-				<!-- Modal-Footer-->
-				<div class="modal-footer">
-					<div class="signature-erase wpeo-button button-grey">
-						<span><i class="fas fa-eraser"></i> <?php echo $langs->trans('Erase'); ?></span>
-					</div>
-					<div class="wpeo-button button-grey modal-close">
-						<span><?php echo $langs->trans('Cancel'); ?></span>
-					</div>
-					<div class="signature-validate wpeo-button button-primary" value="<?php echo $ext_society_responsible->id ?>">
-						<span><?php echo $langs->trans('Validate'); ?></span>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-<?php else : ?>
-<img src='<?php echo $ext_society_responsible->signature ?>' width="100px" height="100px" style="border: #0b419b solid 2px">
-<?php endif; ?>
-<?php
-print '</td>';
-print '<td>';
-print $ext_society_responsible->status;
+print '</td><td class="center">';
+require __DIR__ . "/core/tpl/digiriskdolibarr_signature_view.tpl.php";
+print '</td><td>';
+print $element->getLibStatut(5);
 print '</td></tr>';
+
+print '</tr>';
+print '</table>';
+print '<br>';
+
+print load_fiche_titre($langs->trans("SignatureIntervenants"), '', '');
+
+print '<table class="border centpercent tableforfield">';
+print '<tr class="liste_titre">';
+print '<td>'.$langs->trans("Role").'</td>';
+print '<td>'.$langs->trans("Name").'</td>';
+print '<td class="center">'.$langs->trans("Action").'</td>';
+print '<td>'.$langs->trans("Status").'</td>';
+print '</tr>'."\n";
 
 //External Society Intervenants -- Intervenants Société extérieure
 $j = 1;
 $ext_society_intervenants = $signatory->fetchSignatory('PP_EXT_SOCIETY_INTERVENANTS', $id);
 if (is_array($ext_society_intervenants) && !empty ($ext_society_intervenants) && $ext_society_intervenants > 0) {
-	foreach ($ext_society_intervenants as $ext_society_intervenant) {
+	foreach ($ext_society_intervenants as $element) {
 		print '<tr class="oddeven"><td>';
 		print $langs->trans("ExtSocietyIntervenants") . ' ' . $j;
-		print '</td>';
-
-		print '<td>';
-		$contact->fetch($ext_society_intervenant->element_id);
+		print '</td><td>';
+		$contact->fetch($element->element_id);
 		print $contact->getNomUrl(1);
-		print '</td>';
-
-		print '<td class="center">';
-		if (empty($ext_society_intervenant->signature)) : ?>
-			<div class="wpeo-button button-blue wpeo-modal-event modal-signature-open modal-open" value="<?php echo $ext_society_intervenant->id ?>">
-				<span><?php echo $langs->trans('Sign'); ?></span>
-			</div>
-
-			<div class="modal-signature" value="<?php echo $ext_society_intervenant->id ?>">
-				<div class="wpeo-modal modal-signature" id="modal-signature<?php echo $ext_society_intervenant->id ?>">
-					<div class="modal-container wpeo-modal-event">
-						<!-- Modal-Header-->
-						<div class="modal-header">
-							<h2 class="modal-title"><?php echo $langs->trans('Signature'); ?></h2>
-							<div class="modal-close"><i class="fas fa-times"></i></div>
-						</div>
-						<!-- Modal-ADD Signature Content-->
-						<div class="modal-content" id="#modalContent">
-							<canvas style="height: 95%; width: 95%; border: #0b419b solid 2px">
-								<?php  echo $ext_society_intervenant->signature ?>
-							</canvas>
-						</div>
-						<!-- Modal-Footer-->
-						<div class="modal-footer">
-							<div class="signature-erase wpeo-button button-grey">
-								<span><i class="fas fa-eraser"></i> <?php echo $langs->trans('Erase'); ?></span>
-							</div>
-							<div class="wpeo-button button-grey modal-close">
-								<span><?php echo $langs->trans('Cancel'); ?></span>
-							</div>
-							<div class="signature-validate wpeo-button button-primary" value="<?php echo $ext_society_intervenant->id ?>">
-								<span><?php echo $langs->trans('Validate'); ?></span>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		<?php else : ?>
-			<div class="wpeo-modal-event modal-signature-open modal-open" value="<?php echo $ext_society_intervenant->id ?>">
-				<img src='<?php echo $ext_society_intervenant->signature ?>' width="100px" height="100px" style="border: #0b419b solid 2px">
-			</div>
-
-			<div class="modal-signature" value="<?php echo $ext_society_intervenant->id ?>">
-				<div class="wpeo-modal modal-signature" id="modal-signature<?php echo $ext_society_intervenant->id ?>">
-					<div class="modal-container wpeo-modal-event">
-						<!-- Modal-Header-->
-						<div class="modal-header">
-							<h2 class="modal-title"><?php echo $langs->trans('Signature'); ?></h2>
-							<div class="modal-close"><i class="fas fa-times"></i></div>
-						</div>
-						<!-- Modal-ADD Signature Content-->
-						<div class="modal-content" id="#modalContent">
-							<input type="hidden" id="signature_data<?php echo $ext_society_intervenant->id ?>" value="<?php echo $ext_society_intervenant->signature ?>">
-							<canvas style="height: 95%; width: 95%; border: #0b419b solid 2px"></canvas>
-						</div>
-						<!-- Modal-Footer-->
-						<div class="modal-footer">
-							<div class="signature-erase wpeo-button button-grey">
-								<span><i class="fas fa-eraser"></i> <?php echo $langs->trans('Erase'); ?></span>
-							</div>
-							<div class="wpeo-button button-grey modal-close">
-								<span><?php echo $langs->trans('Cancel'); ?></span>
-							</div>
-							<div class="signature-validate wpeo-button button-primary" value="<?php echo $ext_society_intervenant->id ?>">
-								<span><?php echo $langs->trans('Validate'); ?></span>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		<?php endif; ?>
-		<?php print '</td>';
-
-		print '<td>';
-		print $ext_society_intervenant->status;
-		print '</td>';
-		print '</tr>';
-
+		print '</td><td class="center">';
+		require __DIR__ . "/core/tpl/digiriskdolibarr_signature_view.tpl.php";
+		print '</td><td>';
+		print $element->getLibStatut(5);
+		print '</td></tr>';
 		$j++;
 	}
 }else {
@@ -374,10 +216,7 @@ if (is_array($ext_society_intervenants) && !empty ($ext_society_intervenants) &&
 print '</tr>';
 print '</table>';
 print '</div>';
-print '</div>';
 
-
-dol_fiche_end();
 // End of page
 llxFooter();
 $db->close();
