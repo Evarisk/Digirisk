c5ff09127a5044caf73b2d1dbd81252154daaced
David <theo.david@evarisk.com>
2021-09-10 10:55:44 +0200

    [PreventionPlanSignature] fix: remove delete attendant in status 2

--- core/tpl/digiriskdolibarr_signature_action_view.tpl.php
+++ core/tpl/digiriskdolibarr_signature_action_view.tpl.php
@@ -27,19 +27,22 @@
 					</form>
 				</li>
 		<?php endif; ?>
-		<?php if ($element->role == 'PP_EXT_SOCIETY_INTERVENANTS') : ?>
-			<li class="dropdown-item">
-				<?php print '<form method="POST" action="'.$_SERVER["PHP_SELF"].'">';
-				print '<input type="hidden" name="token" value="'.newToken().'">';
-				print '<input type="hidden" name="action" value="deleteAttendant">';
-				print '<input type="hidden" name="signatoryID" value="'.$element->id.'">';
-				print '<input type="hidden" name="backtopage" value="'.$backtopage.'">'; ?>
-				<button type="submit" name="deleteAttendant" id="deleteAttendant" class="attendant-delete wpeo-button button-primary" value="<?php echo $element->id ?>">
-					<span><i class="fas fa-trash"></i> <?php echo $langs->trans('DeleteAttendant'); ?></span>
-				</button>
-				</form>
-			</li>
+		<?php if ($object->status == 1) : ?>
+			<?php if ($element->role == 'PP_EXT_SOCIETY_INTERVENANTS') : ?>
+				<li class="dropdown-item">
+					<?php print '<form method="POST" action="'.$_SERVER["PHP_SELF"].'">';
+					print '<input type="hidden" name="token" value="'.newToken().'">';
+					print '<input type="hidden" name="action" value="deleteAttendant">';
+					print '<input type="hidden" name="signatoryID" value="'.$element->id.'">';
+					print '<input type="hidden" name="backtopage" value="'.$backtopage.'">'; ?>
+					<button type="submit" name="deleteAttendant" id="deleteAttendant" class="attendant-delete wpeo-button button-primary" value="<?php echo $element->id ?>">
+						<span><i class="fas fa-trash"></i> <?php echo $langs->trans('DeleteAttendant'); ?></span>
+					</button>
+					</form>
+				</li>
+			<?php endif; ?>
 		<?php endif; ?>
+
 	</ul>
 </div>
 
