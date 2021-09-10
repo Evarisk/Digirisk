2b06b951fb5824b0f0db8c514378cd59b1dd9926
noé <noe@sellam.fr>
2021-09-10 11:45:53 +0200

    [PreventionPlan] fix: Changed code for Validation button
    html tag for the button were <span></a>, changed to a more consistant writing

--- preventionplan_card.php
+++ preventionplan_card.php
@@ -1163,7 +1163,7 @@ if ((empty($action) || ($action != 'create' && $action != 'edit')))
 				print '<a class="butAction" id="actionButtonEdit" href="' . $_SERVER["PHP_SELF"] . '?id=' . $object->id . '&action=edit">' . $langs->trans("Modify") . '</a>';
 			}
 			if ($object->status == 1) {
-				print '<span class="butAction" id="actionButtonPendingSignature" href="' . $_SERVER["PHP_SELF"] . '?id=' . $object->id . '&action=setPendingSignature">' . $langs->trans("Validate") . '</a>';
+				print '<span class="butAction" id="actionButtonPendingSignature" href="' . $_SERVER["PHP_SELF"] . '?id=' . $object->id . '&action=setPendingSignature">' . $langs->trans("Validate") . '</span>';
 			} elseif ($object->status == 2) {
 				print '<span class="butAction" id="actionButtonInProgress" href="' . $_SERVER["PHP_SELF"] . '?id=' . $object->id . '&action=setInProgress">' . $langs->trans("ReOpenDigi") . '</span>';
 
