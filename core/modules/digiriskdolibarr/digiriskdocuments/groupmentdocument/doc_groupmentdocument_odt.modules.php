bb653f6f3b9452bd0e266bd78919327f25fb6324
David <theo.david@evarisk.com>
2021-09-23 18:28:46 +0200

    [DigiriskDocuments] remove: other eprt

--- core/modules/digiriskdolibarr/digiriskdocuments/groupmentdocument/doc_groupmentdocument_odt.modules.php
+++ core/modules/digiriskdolibarr/digiriskdocuments/groupmentdocument/doc_groupmentdocument_odt.modules.php
@@ -341,11 +341,6 @@ class doc_groupmentdocument_odt extends ModeleODTGroupmentDocument
 														$listlines->setImage($key, $val);
 													} else {
 														if ($val == '') {
-															echo '<pre>';
-															print_r('oui');
-															print_r($val);
-															print_r('iou');
-															echo '</pre>';
 															$listlines->setVars($key, $langs->trans('NoData'), true, 'UTF-8');
 														} else {
 															$listlines->setVars($key, $val, true, 'UTF-8');
@@ -397,11 +392,6 @@ class doc_groupmentdocument_odt extends ModeleODTGroupmentDocument
 											$listlines->setImage($key, $val);
 										} else {
 											if ($val == '') {
-												echo '<pre>';
-												print_r('oui');
-												print_r($val);
-												print_r('iou');
-												echo '</pre>';
 												$listlines->setVars($key, $langs->trans('NoData'), true, 'UTF-8');
 											} else {
 												$listlines->setVars($key, $val, true, 'UTF-8');
@@ -445,11 +435,6 @@ class doc_groupmentdocument_odt extends ModeleODTGroupmentDocument
 											$listlines->setImage($key, $val);
 										} else {
 											if ($val == '') {
-												echo '<pre>';
-												print_r('oui');
-												print_r($val);
-												print_r('iou');
-												echo '</pre>';
 												$listlines->setVars($key, $langs->trans('NoData'), true, 'UTF-8');
 											} else {
 												$listlines->setVars($key, $val, true, 'UTF-8');
