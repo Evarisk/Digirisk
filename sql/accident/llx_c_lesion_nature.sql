e18a2f7667ac9169c087a62688b9d9d1007b2eed
Nicolas <nicolas@eoxia.com>
2021-11-09 15:53:44 +0100

    [SQL] add: Data model for accident

---
+++ sql/accident/llx_c_lesion_nature.sql
@@ -0,0 +1,23 @@
+-- Copyright (C) 2021 EOXIA <dev@eoxia.com>
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see https://www.gnu.org/licenses/.
+
+CREATE TABLE llx_c_lesion_nature(
+	rowid       integer AUTO_INCREMENT PRIMARY KEY NOT NULL,
+	entity      integer default 1,
+	ref         varchar(128),
+	label       varchar(255),
+	description text,
+	active      tinyint(4) DEFAULT 0
+) ENGINE=innodb;
