7d0ba1c27175ed310d68f292ec95fd3110347c39
Nicolas <nicolas@eoxia.com>
2021-11-24 11:46:59 +0100

    [Mod] add: Version 8.5.0

--- core/triggers/interface_99_modDigiriskdolibarr_DigiriskdolibarrTriggers.class.php
+++ core/triggers/interface_99_modDigiriskdolibarr_DigiriskdolibarrTriggers.class.php
@@ -47,7 +47,7 @@ class InterfaceDigiriskdolibarrTriggers extends DolibarrTriggers
 		$this->name = preg_replace('/^Interface/i', '', get_class($this));
 		$this->family = "demo";
 		$this->description = "Digiriskdolibarr triggers.";
-		$this->version = '8.4.1';
+		$this->version = '8.5.0';
 		$this->picto = 'digiriskdolibarr@digiriskdolibarr';
 	}
 
