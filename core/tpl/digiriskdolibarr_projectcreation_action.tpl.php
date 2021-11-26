f720869b98ed652c551555143355dfcf6f04e1aa
Théo David <theo.evarisk@gmail.com>
2021-11-26 10:49:08 +0100

    [ApiUser] fix: set email to empty string

--- core/tpl/digiriskdolibarr_projectcreation_action.tpl.php
+++ core/tpl/digiriskdolibarr_projectcreation_action.tpl.php
@@ -141,6 +141,7 @@ if ( $conf->global->DIGIRISKDOLIBARR_USERAPI_SET ==  0 ) {
 	$usertmp->lastname  = 'API';
 	$usertmp->firstname = 'REST';
 	$usertmp->login     = 'USERAPI';
+	$usertmp->email     = '';
 	$usertmp->entity    = $conf->entity;
 	$usertmp->setPassword($user);
 	$usertmp->api_key = getRandomPassword(true);
