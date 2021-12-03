5d16b63ad6794d61766b4e8c8437a02cccc32bad
eoxia-amandine <amandine@eoxia.com>
2021-12-03 10:39:26 +0100

    [Config] fix: template of charts js in security page

--- core/tpl/digiriskdolibarr_configuration_gauge_view.tpl.php
+++ core/tpl/digiriskdolibarr_configuration_gauge_view.tpl.php
@@ -1,5 +1,11 @@
-<div class="chart-container" style=" width:150px">
-	<canvas class="" id="advancementGauge" width="40" height="40" style=" float:right; width:100px !important"></canvas>
+<div class="chart-container" style=" width:50px">
+	<div class="wpeo-gridlayout grid-2">
+		<canvas class="" id="advancementGauge" width="40" height="40" style="width:50px !important"></canvas>
+		<h3 class="">
+			<?php echo bcdiv((($counter / $maxnumber) * 100), 1, 2) . '%' ?>
+		</h3>
+	</div>
+
 	<script>
 		const ctx = document.getElementById('advancementGauge').getContext('2d');
 		const advancementGauge = new Chart(ctx, {
@@ -15,12 +21,12 @@
 					label: '# of Votes',
 					data: [<?php echo bcdiv(((1 - $counter / $maxnumber) * 100), 1, 2) ;?>, <?php echo bcdiv((($counter / $maxnumber) * 100), 1, 2) ;?>],
 					backgroundColor: [
-						'rgba(255, 99, 132, 0.2)',
-						'rgba(54, 162, 235, 0.2)',
+						'rgba(108, 108, 108, 0.4)',
+						'rgba(13, 138, 255, 0.8)',
 					],
 					borderColor: [
-						'rgba(255, 99, 132, 1)',
-						'rgba(54, 162, 235, 1)',
+						'rgba(108, 108, 108, 0.8)',
+						'rgba(13, 138, 255, 1)',
 					],
 					borderWidth: 1,
 					radius: 12
@@ -29,7 +35,4 @@
 			width: 100
 		});
 	</script>
-	<h3 class="">
-		<?php echo bcdiv((($counter / $maxnumber) * 100), 1, 2) . '%' ?>
-	</h3>
 </div>
