662fd8fce34b065967b70d079275f1a77aca9835
Nicolas Domenech <nicolas@eoxia.com>
2021-12-14 15:39:48 +0100

    [Mod] add: extrafield user for Professional qualification

--- core/modules/modDigiriskDolibarr.class.php
+++ core/modules/modDigiriskDolibarr.class.php
@@ -1126,6 +1126,7 @@ class modDigiriskdolibarr extends DolibarrModules
 		//Used for data import from Digirisk Wordpress
 		$extra_fields->addExtraField( 'wp_digi_id', $langs->trans("WPDigiID"), 'int', 100, '', 'digiriskdolibarr_digiriskelement', 1, 0, '', '', '', '', 0);
 
+		$extra_fields->addExtraField( 'professional_qualification', $langs->trans("ProfessionalQualification"), 'varchar', 990, 255, 'user', 0, 0, '', 'a:1:{s:7:"options";a:1:{s:0:"";N;}}', 1, '', 1, '', '', 0, 'digiriskdolibarr');
 		$extra_fields->addExtraField( 'contract_type', $langs->trans("ContractType"), 'select', 1000, '', 'user', 0, 0, '', 'a:1:{s:7:"options";a:5:{i:1;s:3:"CDI";i:2;s:3:"CDD";i:3;s:18:"Apprentice/Student";i:4;s:7:"Interim";i:5;s:5:"Other";}}', 1, '', 1, '', '', 0, 'digiriskdolibarr');
 
 		if ($conf->global->MAIN_EXTRAFIELDS_USE_SELECT2 == 0) {
