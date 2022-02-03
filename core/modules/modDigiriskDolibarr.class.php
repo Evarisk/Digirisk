f2c20e734dde9bb443e395f759a4d6d8cce8c7c1
Theo <theo.evarisk@gmail.com>
2022-02-03 12:34:01 +0100

    [DigiriskElement] fix: remove WPdigi extrafield visibility

--- core/modules/modDigiriskDolibarr.class.php
+++ core/modules/modDigiriskDolibarr.class.php
@@ -1442,7 +1442,7 @@ class modDigiriskdolibarr extends DolibarrModules
 		$extra_fields->addExtraField('fk_accident', $langs->trans("fk_accident"), 'sellist', 1020, '', 'projet_task', 0, 0, '', 'a:1:{s:7:"options";a:1:{s:50:"digiriskdolibarr_accident:ref:rowid::entity = $ENTITY$";N;}a:1:{s:7:"options";a:1:{s:50:"digiriskdolibarr_risk:ref:rowid::entity = $ENTITY$";N;}}}', '', '', 1);
 
 		//Used for data import from Digirisk Wordpress
-		$extra_fields->update('wp_digi_id', $langs->trans("WPDigiID"), 'int', 100, 'digiriskdolibarr_digiriskelement', 0, 0, 1020, '', '', '', 1);
+		$extra_fields->update('wp_digi_id', $langs->trans("WPDigiID"), 'int', 100, 'digiriskdolibarr_digiriskelement', 0, 0, 1020, '', '', '', 0);
 		$extra_fields->addExtraField('wp_digi_id', $langs->trans("WPDigiID"), 'int', 100, '', 'digiriskdolibarr_digiriskelement', 0, 0, '', '', '', '', 0);
 		$extra_fields->addExtraField('entity', $langs->trans("Entity"), 'int', 100, '', 'digiriskdolibarr_digiriskelement', 0, 0, '', '', '', '', 0);
 
