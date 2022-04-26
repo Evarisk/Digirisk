f91dcffe817c56c0d08ec68e98c288bbe2083d2e
Théo David <theo.evarisk@gmail.com>
2022-04-26 15:59:47 +0200

    [Security] fix: security checks

--- core/tpl/digirisk_security_checks.php
+++ core/tpl/digirisk_security_checks.php
@@ -1,6 +1,9 @@
 <?php
-if ($object->entity != $conf->entity) {
-	$urltogo = dol_buildpath('/custom/digiriskdolibarr/digiriskdolibarrindex.php?idmenu=1319&mainmenu=digiriskdolibarr&leftmenu=', 1);
-	header("Location: " . $urltogo);
-	exit;
+if (($object->id > 0)) {
+	if ($object->entity != $conf->entity) {
+		$urltogo = dol_buildpath('/custom/digiriskdolibarr/digiriskdolibarrindex.php?idmenu=1319&mainmenu=digiriskdolibarr&leftmenu=', 1);
+		header("Location: " . $urltogo);
+		exit;
+	}
 }
+
