ca2e394167009e5f2d0ccb60a6f7526884d32d72
Nicolas <nicolas@eoxia.com>
2022-04-28 16:40:58 +0200

    [Security] fix: add check multicompany

--- core/tpl/digirisk_security_checks.php
+++ core/tpl/digirisk_security_checks.php
@@ -1,5 +1,5 @@
 <?php
-if (($object->id > 0)) {
+if (($object->id > 0) && $conf->multicompany->enabled) {
 	if ($object->entity != $conf->entity) {
 		$urltogo = dol_buildpath('/custom/digiriskdolibarr/digiriskdolibarrindex.php?idmenu=1319&mainmenu=digiriskdolibarr&leftmenu=', 1);
 		header("Location: " . $urltogo);
