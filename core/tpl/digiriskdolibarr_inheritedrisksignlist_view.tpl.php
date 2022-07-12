c1cd22f3d224eb916a6fe0a535117aecd8911828
Nicolas <nicolas@eoxia.com>
2022-07-12 23:19:14 +0200

    [RiskSign] add: remove select checkbox for mass actions

--- core/tpl/digiriskdolibarr_inheritedrisksignlist_view.tpl.php
+++ core/tpl/digiriskdolibarr_inheritedrisksignlist_view.tpl.php
@@ -124,7 +124,6 @@ include DOL_DOCUMENT_ROOT . '/core/tpl/extrafields_list_search_param.tpl.php';
 
 // List of mass actions available
 $arrayofmassactions                                       = array();
-if ($permissiontodelete) $arrayofmassactions['predelete'] = '<span class="fa fa-trash paddingrightonly"></span>' . $langs->trans("Delete");
 
 if ($action != 'list') {
 	$massactionbutton = $form->selectMassAction('', $arrayofmassactions);
