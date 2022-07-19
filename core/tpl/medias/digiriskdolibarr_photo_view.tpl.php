0da96949a9cc139f8079c3aa537ba4d9be3a1777
Théo David <theo.evarisk@gmail.com>
2022-07-19 17:43:23 +0200

    [RiskList] add: media preview

--- core/tpl/medias/digiriskdolibarr_photo_view.tpl.php
+++ core/tpl/medias/digiriskdolibarr_photo_view.tpl.php
@@ -50,9 +50,15 @@ if (empty($conf) || ! is_object($conf)) {
 					<span class="floatleft inline-block valignmiddle divphotoref risk-evaluation-photo-single">
 						<input class="filepath-to-riskassessment filepath-to-riskassessment-<?php echo $risk->id > 0 ? $risk->id : 'new' ?>" type="hidden" value="<?php echo $pathToThumb ?>">
 						<input class="filename" type="hidden" value="">
-						 <?php 	print '<img width="40" class="photo clicked-photo-preview" src="' . DOL_URL_ROOT . '/custom/digiriskdolibarr/documents/viewimage.php?modulepart=digiriskdolibarr&entity=' . $cotation->entity . '&file=' . urlencode($cotation->element . '/' . $cotation->ref . '/thumbs/' . preg_replace('/\./', '_small.', $cotation->photo)) . '" >';
-							?>
+
+						 <?php
+							 $urladvanced = getAdvancedPreviewUrl('ecm', 'digiriskdolibarr/medias/' .  $cotation->photo, 0, 'entity=' . $conf->entity);
+
+							 print '<a class="clicked-photo-preview" href="'. $urladvanced .'"><i class="fas fa-2x fa-search-plus"></i>';
+							 print '<img width="40" class="photo clicked-photo-preview" src="' . DOL_URL_ROOT . '/custom/digiriskdolibarr/documents/viewimage.php?modulepart=digiriskdolibarr&entity=' . $cotation->entity . '&file=' . urlencode($cotation->element . '/' . $cotation->ref . '/thumbs/' . preg_replace('/\./', '_small.', $cotation->photo)) . '" ></a>';
+						 ?>
 					</span>
+
 					<?php
 				} else {
 					?>
