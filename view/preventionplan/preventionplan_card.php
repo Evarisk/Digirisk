d8f7c566aba531a141c36c99073d56f8d505e17f
Théo David <theo.evarisk@gmail.com>
2022-07-21 15:30:46 +0200

    [PreventionPlan] fix: select location no more exclude digiriskelement with object id

--- view/preventionplan/preventionplan_card.php
+++ view/preventionplan/preventionplan_card.php
@@ -1216,7 +1216,7 @@ if ((empty($action) || ($action != 'create' && $action != 'edit'))) {
 					print '</td>';
 
 					print '<td>';
-					print $digiriskelement->select_digiriskelement_list($item->fk_element, 'fk_element', '', 0, 0, array(), 0, 0, 'minwidth100', GETPOST('id'), false, 1);
+					print $digiriskelement->select_digiriskelement_list($item->fk_element, 'fk_element', '', 0, 0, array(), 0, 0, 'minwidth100', 0, false, 1);
 					print '</td>';
 
 					$coldisplay++;
@@ -1416,7 +1416,7 @@ if ((empty($action) || ($action != 'create' && $action != 'edit'))) {
 
 		if ($permissiontoadd || $permissiontoread) {
 			$genallowed = 1;
-		}	
+		}
 
 		print digiriskshowdocuments($modulepart, $dir_files, $filedir, $urlsource, $genallowed, 0, $defaultmodel, 1, 0, '', $title, '', '', $preventionplandocument, 0, 'remove_file', $object->status == 3 && empty(dol_dir_list($filedir)), $langs->trans('PreventionPlanMustBeLocked'));
 	}
