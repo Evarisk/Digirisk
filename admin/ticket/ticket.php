2a1d067130f0ac3897b187313ec3febd7d40c468
Nicolas Domenech <nicolas@eoxia.com>
2022-08-30 09:47:38 +0200

    [AdminConf] fix: remove logo form qrcode

--- admin/ticket/ticket.php
+++ admin/ticket/ticket.php
@@ -200,29 +200,10 @@ if ($action == 'setTicketSuccessMessage') {
 if ($action == 'generateQRCode') {
 	$data = DOL_DOCUMENT_ROOT . '/custom/digiriskdolibarr/public/ticket/create_ticket.php?entity=' . $conf->entity;
 	$size = '400x400';
-	$entity = $conf->entity > 1 ? '/' . $conf->entity . '/' : '';
-	$logo = DOL_DATA_ROOT . $entity . 'mycompany/logos/' . $conf->global->MAIN_INFO_SOCIETE_LOGO;
 
 	ob_clean();
 
 	$QR = imagecreatefrompng('https://chart.googleapis.com/chart?cht=qr&chld=H|1&chs='.$size.'&chl='.urlencode($data));
-
-	if($conf->global->MAIN_INFO_SOCIETE_LOGO != ''){
-		$logo = imagecreatefromstring(file_get_contents($logo));
-
-		$QR_width = imagesx($QR);
-		$QR_height = imagesy($QR);
-
-		$logo_width = imagesx($logo);
-		$logo_height = imagesy($logo);
-
-		// Scale logo to fit in the QR Code
-		$logo_qr_width = $QR_width/3;
-		$scale = $logo_width/$logo_qr_width;
-		$logo_qr_height = $logo_height/$scale;
-
-		imagecopyresampled($QR, $logo, $QR_width/3, $QR_height/3, 0, 0, $logo_qr_width, $logo_qr_height, $logo_width, $logo_height);
-	}
 
 	$targetPath = $conf->digiriskdolibarr->multidir_output[$conf->entity?:1] . "/ticketqrcode/";
 	if (! is_dir($targetPath)) {
@@ -232,7 +213,6 @@ if ($action == 'generateQRCode') {
 	$targetPath = $targetPath . "ticketQRCode.png";
 
 	imagepng($QR,$targetPath);
-
 }
 
 /*
