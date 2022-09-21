e063a120cc73f7717cfb11f2644ab0cac5f00af6
Nicolas Domenech <nicolas@eoxia.com>
2022-09-22 01:17:07 +0200

    #2382 [AdminConf] add: selector anchor on documents conf

--- admin/config/digiriskdocuments.php
+++ admin/config/digiriskdocuments.php
@@ -198,16 +198,6 @@ $morecss  = array("/digiriskdolibarr/css/digiriskdolibarr.css");
 
 llxHeader('', $title, $help_url, '', '', '', $morejs, $morecss);
 
-// Subheader
-$linkback = '<a href="'.($backtopage ? $backtopage : DOL_URL_ROOT.'/admin/modules.php?restore_lastsearch_values=1').'">'.$langs->trans("BackToModuleList").'</a>';
-
-print load_fiche_titre($title, $linkback, 'digiriskdolibarr32px@digiriskdolibarr');
-
-// Configuration header
-$head = digiriskdolibarrAdminPrepareHead();
-print dol_get_fiche_head($head, 'digiriskdocuments', '', -1, "digiriskdolibarr@digiriskdolibarr");
-
-
 $types = array(
 	'LegalDisplay' 				=> 'legaldisplay',
 	'InformationsSharing' 		=> 'informationssharing',
@@ -234,9 +224,22 @@ $pictos = array(
 	'Ticket'	 				=> '<i class="fa fa-ticket"></i> '
 );
 
+// Subheader
+$selectorAnchor = '<select onchange="location = this.value;">';
+foreach ($types as $type => $documentType) {
+	$selectorAnchor .= '<option value="#' . $langs->trans($type) . '">' . $langs->trans($type) . '</option>';
+}
+$selectorAnchor .= '<select>';
+
+print load_fiche_titre($title, $selectorAnchor, 'digiriskdolibarr32px@digiriskdolibarr');
+
+// Configuration header
+$head = digiriskdolibarrAdminPrepareHead();
+print dol_get_fiche_head($head, 'digiriskdocuments', '', -1, "digiriskdolibarr@digiriskdolibarr");
+
 foreach ($types as $type => $documentType) {
 
-	print load_fiche_titre($pictos[$type] . $langs->trans($type), '', '');
+	print load_fiche_titre($pictos[$type] . $langs->trans($type), '', '', 0, $langs->trans($type));
 	print '<hr>';
 
 	$trad = 'Digirisk' . $type . 'DocumentNumberingModule';
