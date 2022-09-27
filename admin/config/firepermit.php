016667246ec8101f4852b9327e096aebac2499f2
David <theo.david@evarisk.com>
2022-09-27 17:01:41 +0200

    #2338 [PreventionPlan] fix: email no more required for master worker

--- admin/config/firepermit.php
+++ admin/config/firepermit.php
@@ -105,14 +105,6 @@ if ($action == 'setmodFirePermitDet') {
 if ($action == 'setMaitreOeuvre') {
 	$maitre_oeuvre_id = GETPOST('maitre_oeuvre');
 
-	if ($maitre_oeuvre_id > 0) {
-		$usertmp->fetch($maitre_oeuvre_id);
-		if ( ! dol_strlen($usertmp->email)) {
-			setEventMessages($langs->trans('ErrorNoEmailForMaitreOeuvre', $langs->transnoentitiesnoconv('MaitreOeuvre')) . ' : ' . '<a target="_blank" href="' . dol_buildpath('/user/card.php?id=' . $usertmp->id, 2) . '">' . $usertmp->lastname . ' ' . $usertmp->firstname . '</a>', null, 'errors');
-			$error++;
-		}
-	}
-
 	if ( ! $error) {
 		$constforval = 'DIGIRISKDOLIBARR_' . strtoupper($type) . "_MAITRE_OEUVRE";
 		dolibarr_set_const($db, $constforval, $maitre_oeuvre_id, 'integer', 0, '', $conf->entity);
