3c59d6cb50df63cad12b085e84a1e2b377153f63
Nicolas Domenech <nicolas@eoxia.com>
2023-01-05 16:17:44 +0100

    #2903 [DigiriskElementDocument] fix: add label in title for digiriskelement

--- core/modules/digiriskdolibarr/digiriskdocuments/digiriskelementdocument/modules_digiriskelementdocument.php
+++ core/modules/digiriskdolibarr/digiriskdocuments/digiriskelementdocument/modules_digiriskelementdocument.php
@@ -97,7 +97,7 @@ abstract class ModeleODTDigiriskElementDocument extends CommonDocGenerator
 			$societyname = preg_replace('/\./', '_', $conf->global->MAIN_INFO_SOCIETE_NOM);
 
 			$date     = dol_print_date(dol_now(), 'dayxcard');
-			$filename = $date . '_' . $digiriskelement->ref . '_' . $objectref . '_' . $societyname . '.odt';
+			$filename = $date . '_' . $digiriskelement->ref . '_' . $objectref . '_' . $digiriskelement->label . '_' . $societyname . '.odt';
 			$filename = str_replace(' ', '_', $filename);
 			$filename = dol_sanitizeFileName($filename);
 			$filename = preg_replace('/[’‘‹›‚]/u', '', $filename);
