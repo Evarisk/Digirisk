83cd8ddebbefa45a4d5960d48f645d702bbd0758
Théo David <theo.david@evarisk.com>
2023-02-08 14:17:08 +0100

    #2977 [ListingRisksPhoto] add: society name in file name

--- core/modules/digiriskdolibarr/digiriskdocuments/listingrisksphoto/modules_listingrisksphoto.php
+++ core/modules/digiriskdolibarr/digiriskdocuments/listingrisksphoto/modules_listingrisksphoto.php
@@ -111,7 +111,7 @@ abstract class ModeleODTListingRisksPhoto extends CommonDocGenerator
 
 			$date = dol_print_date(dol_now(), 'dayxcard');
 			if ( ! empty($digiriskelement)) {
-				$filename = $date . '_' . $digiriskelement->ref . '_' . $objectref . '_' . $digiriskelement->label . '.odt';
+				$filename = $date . '_' . $digiriskelement->ref . '_' . $objectref . '_' . $digiriskelement->label . '_' . $societyname . '.odt';
 			} else {
 				$filename = $date . '_' . $objectref . '_' . $societyname . '.odt';
 			}
