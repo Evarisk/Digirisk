02976ce6a1b207e781c6eeefd0dd1251ad935501
Théo David <theo.david@evarisk.com>
2023-02-09 11:26:13 +0100

    #2979 [DigiriskDocuments] fix: pdf generation action template

--- class/actions_digiriskdolibarr.class.php
+++ class/actions_digiriskdolibarr.class.php
@@ -487,57 +487,12 @@ class ActionsDigiriskdolibarr
 				}
 			}
 
+			$upload_dir = $conf->digiriskdolibarr->multidir_output[isset($conf->entity) ? $conf->entity : 1];
+
 			// Action to generate pdf from odt file
-			if (GETPOST('action') == 'pdfGeneration') {
-				global $langs;
+			include_once DOL_DOCUMENT_ROOT . '/custom/digiriskdolibarr/core/tpl/documents/digiriskdolibarr_manual_pdf_generation_action.tpl.php';
 
-				$upload_dir = $conf->digiriskdolibarr->multidir_output[isset($conf->entity) ? $conf->entity : 1];
-				$filename = GETPOST('file');
-
-				$file = $upload_dir . '/' . $filename;
-
-
-				$fileInfos = pathinfo($filename);
-				$pdfName   = $fileInfos['filename'] . '.pdf';
-
-				// Open and load template
-				require_once ODTPHP_PATH . 'odf.php';
-				try {
-					$odfHandler = new odf(
-						$file,
-						array(
-							'PATH_TO_TMP'	  => $conf->digiriskdolibarr->dir_temp,
-							'ZIP_PROXY'		  => 'PclZipProxy', // PhpZipProxy or PclZipProxy. Got "bad compression method" error when using PhpZipProxy.
-							'DELIMITER_LEFT'  => '{',
-							'DELIMITER_RIGHT' => '}'
-						)
-					);
-				} catch (Exception $e) {
-					$error = $e->getMessage();
-					dol_syslog($e->getMessage(), LOG_INFO);
-
-					return -1;
-				}
-
-				// Write new file
-				if ( ! empty($conf->global->MAIN_ODT_AS_PDF) && $conf->global->DIGIRISKDOLIBARR_MANUAL_PDF_GENERATION > 0) {
-					try {
-						$odfHandler->exportAsAttachedPDF($file);
-						setEventMessages($langs->trans("FileGenerated") . ' - ' . $pdfName, null);
-					} catch (Exception $e) {
-						$error = $e->getMessage();
-						setEventMessages($langs->transnoentities('FileCouldNotBeGeneratedInPDF') . '<br>' . $langs->transnoentities('CheckDocumentationToEnablePDFGeneration'), null, 'errors');
-						dol_syslog($e->getMessage(), LOG_INFO);
-					}
-				} else {
-					try {
-						$odfHandler->saveToDisk($file);
-					} catch (Exception $e) {
-						$error = $e->getMessage();
-						dol_syslog($e->getMessage(), LOG_INFO);
-						return -1;
-					}
-				}
+			if ($action == 'pdfGeneration') {
 				$urltoredirect = $_SERVER['REQUEST_URI'];
 				$urltoredirect = preg_replace('/#pdfGeneration$/', '', $urltoredirect);
 				$urltoredirect = preg_replace('/action=pdfGeneration&?/', '', $urltoredirect); // To avoid infinite loop
--- view/firepermit/firepermit_card.php
+++ view/firepermit/firepermit_card.php
@@ -509,53 +509,8 @@ if (empty($reshook)) {
 	}
 
 	// Action to generate pdf from odt file
-	if ($action == 'pdfGeneration') {
-		$filename = GETPOST('file');
+	include_once DOL_DOCUMENT_ROOT . '/custom/digiriskdolibarr/core/tpl/documents/digiriskdolibarr_manual_pdf_generation_action.tpl.php';
 
-		$file = $upload_dir . '/' . $filename;
-
-		// Open and load template
-		require_once ODTPHP_PATH . 'odf.php';
-		try {
-			$odfHandler = new odf(
-				$file,
-				array(
-					'PATH_TO_TMP'	  => $conf->digiriskdolibarr->dir_temp,
-					'ZIP_PROXY'		  => 'PclZipProxy', // PhpZipProxy or PclZipProxy. Got "bad compression method" error when using PhpZipProxy.
-					'DELIMITER_LEFT'  => '{',
-					'DELIMITER_RIGHT' => '}'
-				)
-			);
-		} catch (Exception $e) {
-			$error = $e->getMessage();
-			dol_syslog($e->getMessage(), LOG_INFO);
-
-			return -1;
-		}
-
-		$fileInfos = pathinfo($filename);
-		$pdfName   = $fileInfos['filename'] . '.pdf';
-
-		// Write new file
-		if ( ! empty($conf->global->MAIN_ODT_AS_PDF) && $conf->global->DIGIRISKDOLIBARR_MANUAL_PDF_GENERATION > 0) {
-			try {
-				$odfHandler->exportAsAttachedPDF($file);
-				setEventMessages($langs->trans("FileGenerated") . ' - ' . $pdfName, null);
-			} catch (Exception $e) {
-				$error = $e->getMessage();
-				setEventMessages($langs->transnoentities('FileCouldNotBeGeneratedInPDF') . '<br>' . $langs->transnoentities('CheckDocumentationToEnablePDFGeneration'), null, 'errors');
-				dol_syslog($e->getMessage(), LOG_INFO);
-			}
-		} else {
-			try {
-				$odfHandler->saveToDisk($file);
-			} catch (Exception $e) {
-				$error = $e->getMessage();
-				dol_syslog($e->getMessage(), LOG_INFO);
-				return -1;
-			}
-		}
-	}
 
 	// Delete file in doc form
 	if ($action == 'remove_file' && $permissiontodelete) {
