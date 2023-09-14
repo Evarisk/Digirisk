beaf7c373728f59c2156113db95c4bc8f578cae8
Nicolas Domenech <nicolas@eoxia.com>
2023-09-14 16:42:12 +0200

    #3259 [Core] add: new version number 9.12.0

--- core/triggers/interface_99_modDigiriskdolibarr_DigiriskdolibarrTriggers.class.php
+++ core/triggers/interface_99_modDigiriskdolibarr_DigiriskdolibarrTriggers.class.php
@@ -72,7 +72,7 @@ class InterfaceDigiriskdolibarrTriggers extends DolibarrTriggers
 		$this->name        = preg_replace('/^Interface/i', '', get_class($this));
 		$this->family      = "demo";
 		$this->description = "Digiriskdolibarr triggers.";
-		$this->version     = '9.11.1';
+		$this->version     = '9.12.0';
 		$this->picto       = 'digiriskdolibarr@digiriskdolibarr';
 	}
 
