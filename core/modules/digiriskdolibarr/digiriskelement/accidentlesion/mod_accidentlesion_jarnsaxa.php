362924cb199be70282e3d33cca2bc359e62948e1
Nicolas Domenech <nicolas@eoxia.com>
2023-10-16 15:42:57 +0200

    #3411 [Admin] fix: change model name for conf order

--- core/modules/digiriskdolibarr/digiriskelement/accidentlesion/mod_accidentlesion_jarnsaxa.php
+++ core/modules/digiriskdolibarr/digiriskelement/accidentlesion/mod_accidentlesion_jarnsaxa.php
@@ -17,29 +17,29 @@
  */
 
 /**
- * \file    core/modules/digiriskdolibarr/digiriskelement/accidentlesion/mod_accidentlesion_wright.php
+ * \file    core/modules/digiriskdolibarr/digiriskelement/accidentlesion/mod_accidentlesion_jarnsaxa.php
  * \ingroup digiriskdolibarr
- * \brief   File that contains the numbering module rules wright
+ * \brief   File that contains the numbering module rules jarnsaxa
  */
 
 // Load Saturne libraries
 require_once __DIR__ . '/../../../../../../saturne/core/modules/saturne/modules_saturne.php';
 
 /**
- * Class of file that contains the numbering module rules wright
+ * Class of file that contains the numbering module rules jarnsaxa
  */
-class mod_accidentlesion_wright extends CustomModeleNumRefSaturne
+class mod_accidentlesion_jarnsaxa extends CustomModeleNumRefSaturne
 {
     /**
      * @var string Model name
      */
-    public string $name = 'Wright';
+    public string $name = 'Jarnsaxa';
 
     public function __construct()
     {
         global $conf;
 
-        $refMod = $conf->global->DIGIRISKDOLIBARR_ACCIDENTLESION_WRIGHT_ADDON;
+        $refMod = $conf->global->DIGIRISKDOLIBARR_ACCIDENTLESION_JARNSAXA_ADDON;
         if (dol_strlen($refMod)) {
             $refModSplitted = preg_split('/\{/', $refMod);
             if (is_array($refModSplitted) && !empty($refModSplitted)) {
