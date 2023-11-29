c23e9aa187f6bf17daafe620d1513ba373665604
Théo David <theo.david@evarisk.com>
2023-11-29 18:48:26 +0100

    #3564 [Risk] fix: addictive risk category in odt & sql

--- sql/update.sql
+++ sql/update.sql
@@ -235,3 +235,7 @@ INSERT INTO llx_c_accidentinvestigation_attendants_role SELECT * FROM llx_c_acci
 INSERT INTO llx_categorie_accidentinvestigation SELECT * FROM llx_categorie_accident_investigation;
 DROP TABLE llx_c_accident_investigation_attendants_role;
 DROP TABLE llx_categorie_accident_investigation;
+
+-- 9.14.0
+UPDATE llx_digiriskdolibarr_risk SET category = 21 WHERE category = 20;
+UPDATE llx_digiriskdolibarr_risk SET category = 22 WHERE category = 21;
