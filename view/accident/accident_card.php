37c0dd3af11c3d1b31efe1c34e39f032f3cdc013
Théo David <theo.david@evarisk.com>
2023-11-30 11:00:11 +0100

    #3540 [Accident] fix: formconfirm bold

--- view/accident/accident_card.php
+++ view/accident/accident_card.php
@@ -898,13 +898,13 @@ if ((empty($action) || ($action != 'create' && $action != 'edit'))) {
 
     // SetValidated confirmation
     if (($action == 'setValidated' && (empty($conf->use_javascript_ajax) || !empty($conf->dol_use_jmobile))) || (!empty($conf->use_javascript_ajax) && empty($conf->dol_use_jmobile))) {
-        $questionConfirmInfo = '<b>' . $langs->trans('ConfirmValidateObject', $langs->trans('TheAccident'), $langs->transnoentities('LesionsOrWorkStop')) . '</b>';
+        $questionConfirmInfo =  $langs->trans('ConfirmValidateObject', $langs->trans('TheAccident'), $langs->transnoentities('LesionsOrWorkStop'));
         $formConfirm .= $form->formconfirm($_SERVER['PHP_SELF'] . '?id=' . $object->id, $langs->trans('ValidateObject', $langs->trans('TheAccident')), $questionConfirmInfo, 'confirm_validate', '', 'yes', 'actionButtonValidate', 250);
     }
 
     // SetReOpen confirmation
     if (($action == 'setReOpen' && (empty($conf->use_javascript_ajax) || !empty($conf->dol_use_jmobile))) || (!empty($conf->use_javascript_ajax) && empty($conf->dol_use_jmobile))) {
-        $questionConfirmInfo = '<b>' . $langs->trans('ConfirmReOpenObject', $langs->trans('TheAccident')) . '</b>';
+        $questionConfirmInfo = $langs->trans('ConfirmReOpenObject', $langs->trans('TheAccident'));
         $formConfirm .= $form->formconfirm($_SERVER['PHP_SELF'] . '?id=' . $object->id, $langs->trans('ValidateObject', $langs->trans('TheAccident')), $questionConfirmInfo, 'confirm_setReopened', '', 'yes', 'actionButtonReOpen', 250);
     }
 
