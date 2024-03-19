7c8d4692818d0b304730401079ca3dce750cb55f
Charles Delachapelle <charles.delachapelle@evarisk.com>
2024-03-19 16:15:36 +0100

    #3746 [Mod] fix: name of thiazzi mod name

--- core/modules/digiriskdolibarr/digiriskdolibarrdocuments/registerdocument/mod_registerdocument_thiazzi.php
+++ core/modules/digiriskdolibarr/digiriskdolibarrdocuments/registerdocument/mod_registerdocument_thiazzi.php
@@ -1,5 +1,5 @@
 <?php
-/* Copyright (C) 2021-2023 EVARISK <technique@evarisk.com>
+/* Copyright (C) 2021-2024 EVARISK <technique@evarisk.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,18 +17,18 @@
  */
 
 /**
- * \file    core/modules/digiriskdolibarr/digiriskdolibarrdocuments/informationssharing/mod_informationssharing_gridr.php
+ * \file    core/modules/digiriskdolibarr/digiriskdolibarrdocuments/informationssharing/mod_informationssharing_thiazzi.php
  * \ingroup digiriskdolibarr
- * \brief   File that contains the numbering module rules gridr
+ * \brief   File that contains the numbering module rules thiazzi
  */
 
 // Load Saturne libraries
 require_once __DIR__ . '/../../../../../../saturne/core/modules/saturne/modules_saturne.php';
 
 /**
- * Class of file that contains the numbering module rules gridr
+ * Class of file that contains the numbering module rules thiazzi
  */
-class mod_registerdocument_gridr extends CustomModeleNumRefSaturne
+class mod_registerdocument_thiazzi extends CustomModeleNumRefSaturne
 {
     /**
      * @var string Model name
