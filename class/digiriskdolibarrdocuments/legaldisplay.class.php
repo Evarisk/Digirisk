9cd8626f76ef67f34c3135daea19fa526542b8ae
evarisk-kilyan <kilyan.evarisk@gmail.com>
2024-12-04 11:03:21 +0100

    #4171 [LegalDisplay] fix: missing ;

--- class/digiriskdolibarrdocuments/legaldisplay.class.php
+++ class/digiriskdolibarrdocuments/legaldisplay.class.php
@@ -236,7 +236,7 @@ class LegalDisplay extends DigiriskDocuments
 			$json['LegalDisplay']['derogation_schedule']['permanent']                                     = $conf->global->DIGIRISKDOLIBARR_DEROGATION_SCHEDULE_PERMANENT;
 			$json['LegalDisplay']['derogation_schedule']['occasional']                                    = $conf->global->DIGIRISKDOLIBARR_DEROGATION_SCHEDULE_OCCASIONAL;
 			$json['LegalDisplay']['collective_agreement']['title_of_the_applicable_collective_agreement'] = $conf->global->DIGIRISKDOLIBARR_COLLECTIVE_AGREEMENT_TITLE;
-            $idcc = $this->getIDCCByCode($conf->global->DIGIRISKDOLIBARR_COLLECTIVE_AGREEMENT_TITLE)
+            $idcc = $this->getIDCCByCode($conf->global->DIGIRISKDOLIBARR_COLLECTIVE_AGREEMENT_TITLE);
             if (!empty($idcc)) {
                 $json['LegalDisplay']['collective_agreement']['title_of_the_applicable_collective_agreement'] .= ' - ' . $idcc->libelle;
             }
