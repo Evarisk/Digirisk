d0cce5a1e345c383d47176edabf0e641ef4eb525
Nicolas Domenech <nicolas@eoxia.com>
2025-08-29 09:37:45 +0200

    #4574 [Mod] add: new version number 21.1.0

--- core/modules/modDigiriskDolibarr.class.php
+++ core/modules/modDigiriskDolibarr.class.php
@@ -380,7 +380,7 @@ class modDigiriskdolibarr extends DolibarrModules
 		$this->descriptionlong = "Digirisk";
 		$this->editor_name     = 'Evarisk';
 		$this->editor_url      = 'https://evarisk.com';
-		$this->version         = '21.0.1';
+		$this->version         = '21.1.0';
 		$this->const_name      = 'MAIN_MODULE_' . strtoupper($this->name);
 		$this->picto           = 'digiriskdolibarr_color@digiriskdolibarr';
 
