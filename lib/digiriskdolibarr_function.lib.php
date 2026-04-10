1bc2a3fe17af1c367ad85f7943a5465836e97507
Nicolas Domenech <nicolas@eoxia.com>
2026-04-10 10:51:52 +0200

    [Class] core: clean code

--- lib/digiriskdolibarr_function.lib.php
+++ lib/digiriskdolibarr_function.lib.php
@@ -57,7 +57,7 @@ function digirisk_header($title = '', $helpUrl = '', $arrayofjs = [], $arrayofcs
 	if ($conf->global->DIGIRISKDOLIBARR_SHOW_HIDDEN_DIGIRISKELEMENT) {
 		$objects = $object->fetchAll('',  'ranks');
 	} else {
-		$objects = $object->fetchAll('',  'ranks',  0,  0, array('customsql' => 'status > 0 AND entity IN ('. $conf->entity .')'));
+		$objects = $object->fetchAll('',  'ranks',  0,  0, array('customsql' => 't.status > 0 AND t.entity IN ('. $conf->entity .')'));
 	}
 
 	$digiriskElementTree = array();
