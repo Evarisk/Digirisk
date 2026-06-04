f4a452f90fc275b2d10ebbadf967da566c68f1dc
Laurent Magnin <laurent1magnin@gmail.com>
2026-06-04 10:15:33 +0200

    #4792 [DigiriskElementRisk] fix: undefined array key help in arrayfields definition

--- view/digiriskelement/digiriskelement_risk.php
+++ view/digiriskelement/digiriskelement_risk.php
@@ -148,7 +148,7 @@ foreach ($risk->fields as $key => $val) {
             'checked'     => (($visible < 0) ? 0 : 1),
             'enabled'     => ($visible != 3 && dol_eval($val['enabled'], 1)),
             'position'    => $val['position'],
-            'help'        => $val['help']
+            'help'        => $val['help'] ?? ''
         ];
     }
 }
@@ -162,7 +162,7 @@ foreach ($evaluation->fields as $key => $val) {
             'checked'     => (($visible < 0) ? 0 : 1),
             'enabled'     => ($visible != 3 && dol_eval($val['enabled'], 1)),
             'position'    => $val['position'],
-            'help'        => $val['help']
+            'help'        => $val['help'] ?? ''
         ];
     }
 }
