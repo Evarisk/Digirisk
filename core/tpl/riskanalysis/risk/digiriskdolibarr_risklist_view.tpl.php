12d8f6f12288c255dbdf88f65d24be8311fe8da4
Laurent Magnin <laurent1magnin@gmail.com>
2026-07-02 00:38:10 +0200

    UI: Remove character counter in risk and risk assessment modals

--- core/tpl/riskanalysis/risk/digiriskdolibarr_risklist_view.tpl.php
+++ core/tpl/riskanalysis/risk/digiriskdolibarr_risklist_view.tpl.php
@@ -795,7 +795,7 @@ $massactionbutton = $form->selectMassAction('', $arrayofmassactions);
                                 </div>
                             </div>
                             <div class="risk-evaluation-comment">
-                                <span class="title"><i class="fas fa-comment-dots"></i> <?php echo $langs->trans('Comment'); ?> (<span class="char-counter">65535</span> <?php echo $langs->trans('CharRemaining'); ?>)</span>
+                                <span class="title"><i class="fas fa-comment-dots"></i> <?php echo $langs->trans('Comment'); ?></span>
                                 <?php print '<textarea class="evaluation-comment-textarea" data-maxlength="65535" maxlength="65535" name="evaluationComment' . $risk->id . '" cols="50" rows="' . ROWS_2 . '">' . ('') . '</textarea>' . "\n"; ?>
                             </div>
                         </div>
