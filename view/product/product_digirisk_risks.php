02182589a0b29ce6a5982e86e8effe7c199b6a66
Laurent Magnin <laurent1magnin@gmail.com>
2026-08-17 22:13:17 +0200

    style(fi): use em units to scale custom SVGs identically to FontAwesome

--- view/product/product_digirisk_risks.php
+++ view/product/product_digirisk_risks.php
@@ -369,7 +369,7 @@ if ($permissiontoadd) {
     
     if ($useSvg && $hasSvg) {
         $svgContent = file_get_contents($svgPath);
-        $svgContent = preg_replace('/<svg([^>]*)>/i', '<svg$1 fill="' . dol_escape_htmltag($risksColor) . '" width="20" height="20">', $svgContent);
+        $svgContent = preg_replace('/<svg([^>]*)>/i', '<svg$1 fill="' . dol_escape_htmltag($risksColor) . '" style="width: 1.25em; height: 1.25em; vertical-align: -0.125em;">', $svgContent);
         $svgContent = preg_replace('/fill="[^"]*"/i', 'fill="' . dol_escape_htmltag($risksColor) . '"', $svgContent);
         $iconHtml = '<span style="display:inline-block; margin-right:8px; vertical-align:middle;">' . $svgContent . '</span>';
     } else {
