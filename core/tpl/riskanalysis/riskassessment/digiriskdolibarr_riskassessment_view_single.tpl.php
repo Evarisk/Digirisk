676b5286bdbb73c7045f59e4bcb9ee1673037191
evarisk-kilyan <kilyan.evarisk@gmail.com>
2026-09-09 12:17:55 +0200

    #1912 [Risk] feat: show the user photo instead of the initials when the file exists

--- core/tpl/riskanalysis/riskassessment/digiriskdolibarr_riskassessment_view_single.tpl.php
+++ core/tpl/riskanalysis/riskassessment/digiriskdolibarr_riskassessment_view_single.tpl.php
@@ -21,7 +21,7 @@
 					</span>
 					<span class="risk-evaluation-author">
 						<?php $userAuthor = $usersList[$lastEvaluation->fk_user_creat?:$user->id];
-						echo getNomUrlUser($userAuthor); ?>
+						echo getNomUrlUser($userAuthor, getUserPhotoPicto($userAuthor)); ?>
 					</span>
 				</div>
 				<div class="risk-evaluation-comment wordbreak">
